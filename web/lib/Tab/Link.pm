220f0ab5563324cbd93559fd098a0bd9415c9626
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-03-01 23:21:27 +0000

    yeah

--- web/lib/Tab/Link.pm
+++ web/lib/Tab/Link.pm
@@ -2,7 +2,7 @@ package Tab::Link;
 use base 'Tab::DBI';
 Tab::Link->table('link');
 Tab::Link->columns(Primary => qw/id/);
-Tab::Link->columns(Essential => qw/id name timestamp blurb league active display_order/);
+Tab::Link->columns(Essential => qw/id name timestamp text league active display_order/);
 Tab::Link->has_a(league => 'Tab::League');
 Tab::Link->has_many(news => 'Tab::News', "link");
 
