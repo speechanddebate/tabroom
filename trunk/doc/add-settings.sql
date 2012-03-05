41c36162b28a0b11df1909da9a6639ed18996831
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-03-05 23:25:46 +0000

    Data imports is going on

--- trunk/doc/add-settings.sql
+++ trunk/doc/add-settings.sql
@@ -67,25 +67,50 @@ CREATE TABLE `circuit_setting` (
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 alter table	chapter add coaches varchar(255);
+alter table tiebreak add tourn int;
+create index tourn on tiebreak(tourn);
+
+CREATE TABLE `ballot_student` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `ballot` int(11) DEFAULT NULL,
+    `student` int(11) DEFAULT NULL,
+    `points` int(11) DEFAULT NULL,
+    `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    KEY `student` (`student`),
+    KEY `ballot` (`ballot`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `qualifier` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    entry int default null,
+    name varchar(63) default null,
+    result varchar(127) default null,
+    tourn int default null,
+    `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    KEY `entry` (`entry`),
+    KEY `tourn` (`tourn`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `follower` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account` int(11) DEFAULT NULL,
+  `follower` int(11) DEFAULT NULL,
+  `parent` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `account` (`account`),
+  KEY `follower` (`follower`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `tourn_circuit` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `tourn` int(11) NOT NULL DEFAULT '0',
     `circuit` int(11) NOT NULL DEFAULT '0',
-    `key` varchar(15) NOT NULL DEFAULT '',
-    `value` varchar(127) NOT NULL DEFAULT '',
-    `text` text,
     `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
     KEY `tourn` (`tourn`),
     KEY `circuit` (`circuit`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
-
-alter table tiebreak add tourn int;
-create index tourn on tiebreak(tourn);
-
-
-
-
-
 
