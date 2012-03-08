b6149f28fbc9105488c158394b602caf8114e36c
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-03-08 02:19:31 +0000

    getting there

--- web/lib/Tab/ConcessionPurchase.pm
+++ web/lib/Tab/ConcessionPurchase.pm
@@ -1,6 +1,6 @@
 package Tab::ConcessionPurchase;
 use base 'Tab::DBI';
-Tab::ConcessionPurchase->table('purchase');
+Tab::ConcessionPurchase->table('concession_purchase');
 Tab::ConcessionPurchase->columns(Essential => qw/id concession quantity school timestamp/);
 Tab::ConcessionPurchase->has_a(school => 'Tab::School');
 Tab::ConcessionPurchase->has_a(concession => 'Tab::Concession');
