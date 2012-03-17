9cc78da8564b166fe4af0a1f5c50084276a93c6e
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-03-17 23:56:17 +0000

    Yes maam

---
+++ web/lib/Tab/Webpage.pm
@@ -0,0 +1,13 @@
+package Tab::Webpage;
+use base 'Tab::DBI';
+Tab::Webpage->table('webpage');
+Tab::Webpage->columns(Primary => qw/id/);
+Tab::Webpage->columns(Essential => qw/tourn circuit order title content last_editor posted_on timestamp/);
+
+Tab::Webpage->has_a(tourn => 'Tab::Tourn');
+Tab::Webpage->has_a(circuit => 'Tab::Circuit');
+Tab::Webpage->has_a(last_editor => 'Tab::Account');
+
+__PACKAGE__->_register_datetimes( qw/posted_on/);
+__PACKAGE__->_register_datetimes( qw/timestamp/);
+
