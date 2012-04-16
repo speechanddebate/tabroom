f74139a682ee3a8f13d22d6a382694d70b8958bb
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-04-16 23:28:29 +0000

    Getting there on the main user interface.

--- trunk/web/lib/Tab/FollowAccount.pm
+++ trunk/web/lib/Tab/FollowAccount.pm
@@ -2,6 +2,7 @@ package Tab::FollowAccount;
 use base 'Tab::DBI';
 Tab::FollowAccount->table('follow_account');
 Tab::FollowAccount->columns(Primary => qw/id/);
-Tab::FollowAccount->columns(Essential => qw/account follower cell email domain/);
+Tab::FollowAccount->columns(Essential => qw/account follower parent/);
 Tab::FollowAccount->has_a(account => 'Tab::Account');
 Tab::FollowAccount->has_a(follower => 'Tab::Account');
+
--- trunk/web/lib/Tab/Session.pm
+++ trunk/web/lib/Tab/Session.pm
@@ -1,8 +1,7 @@
 package Tab::Session;
 use base 'Tab::DBI';
 Tab::Session->table('session');
-Tab::Session->columns(All => qw/id account authkey userkey timestamp ip 
-                                limited entry circuit tourn school event/);
+Tab::Session->columns(All => qw/id account authkey userkey timestamp ip limited entry_only circuit tourn school event/);
 
 Tab::Session->has_a(account => 'Tab::Account');
 Tab::Session->has_a(circuit => 'Tab::Circuit');
