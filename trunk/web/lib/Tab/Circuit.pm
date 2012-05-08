8887bdcc625437631ad4a16b4181d777f8e6375b
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-05-08 02:46:27 +0000

    Fixed that really annoying date bug.  Finally

--- trunk/web/lib/Tab/Circuit.pm
+++ trunk/web/lib/Tab/Circuit.pm
@@ -75,7 +75,7 @@ sub setting {
 		tag => $tag
 	);
 
-    if ($value && $value != 0) {
+    if ($value && $value ne 0) {
 
 		if (@existing) {
 
--- trunk/web/lib/Tab/TournCircuit.pm
+++ trunk/web/lib/Tab/TournCircuit.pm
@@ -2,7 +2,7 @@ package Tab::TournCircuit;
 use base 'Tab::DBI';
 Tab::TournCircuit->table('tourn_circuit');
 Tab::TournCircuit->columns(Primary => qw/id/);
-Tab::TournCircuit->columns(Essential => qw/circuit tourn timestamp/);
+Tab::TournCircuit->columns(Essential => qw/circuit tourn timestamp appproved/) ;
 
 Tab::TournCircuit->has_a(tourn => "Tab::Tourn");
 Tab::TournCircuit->has_a(circuit => "Tab::Circuit");
