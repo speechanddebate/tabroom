6031b7a75f067f5577948781b4552f242fbf3867
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-06-18 22:52:56 +0000

    Search function works, email sending done

--- trunk/web/lib/Tab/Email.pm
+++ trunk/web/lib/Tab/Email.pm
@@ -2,8 +2,9 @@ package Tab::Email;
 use base 'Tab::DBI';
 Tab::Email->table('email');
 Tab::Email->columns(Primary => qw/id/);
-Tab::Email->columns(Essential => qw/timestamp subject circuit tourn senton sender sent_to text/);
+Tab::Email->columns(Essential => qw/sender content subject sent_on circuit sent_to tourn timestamp/);
 Tab::Email->has_a(sender => 'Tab::Account');
 Tab::Email->has_a(tourn => 'Tab::Tourn');
-__PACKAGE__->_register_datetimes( qw/senton/);
+
+__PACKAGE__->_register_datetimes( qw/sent_on timestamp/);
 
