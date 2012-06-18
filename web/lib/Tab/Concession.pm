e6a3726aefff22838978e68affd567331e683c84
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-06-18 22:52:56 +0000

    Search function works, email sending done

--- web/lib/Tab/Concession.pm
+++ web/lib/Tab/Concession.pm
@@ -2,11 +2,11 @@ package Tab::Concession;
 use base 'Tab::DBI';
 Tab::Concession->table('concession');
 Tab::Concession->columns(Primary => qw/id/);
-Tab::Concession->columns(Essential => qw/name price tourn deadline/);
-Tab::Concession->columns(Other => qw/description timestamp/);
+Tab::Concession->columns(Essential => qw/name price tourn deadline description timestamp/);
 
 Tab::Concession->has_a(tourn => 'Tab::Tourn');
-
+Tab::Concession->has_many(purchases => 'Tab::ConcessionPurchase', 'concession');
 Tab::Concession->has_many(concession_purchases => 'Tab::ConcessionPurchase', 'concession');
+
 __PACKAGE__->_register_datetimes( qw/timestamp/);
 __PACKAGE__->_register_datetimes( qw/deadline/);
--- web/lib/Tab/ConcessionPurchase.pm
+++ web/lib/Tab/ConcessionPurchase.pm
@@ -1,7 +1,7 @@
 package Tab::ConcessionPurchase;
 use base 'Tab::DBI';
 Tab::ConcessionPurchase->table('concession_purchase');
-Tab::ConcessionPurchase->columns(Essential => qw/id concession quantity school placed timestamp/);
+Tab::ConcessionPurchase->columns(Essential => qw/id concession quantity school placed timestamp fulfilled/);
 Tab::ConcessionPurchase->has_a(school => 'Tab::School');
 Tab::ConcessionPurchase->has_a(concession => 'Tab::Concession');
 __PACKAGE__->_register_datetimes( qw/timestamp/);
