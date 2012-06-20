cd278dbaa74eb786da221f205da814b3429564f1
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-06-20 03:13:23 +0000

    More

---
+++ web/lib/Tab/Housing.pm
@@ -0,0 +1,16 @@
+package Tab::Housing;
+use base 'Tab::DBI';
+Tab::Housing->table('housing');
+Tab::Housing->columns(Primary => "id");
+Tab::Housing->columns(Essential => qw/tourn student judge type waitlist night requested account school timestamp/);
+
+Tab::Housing->has_a(tourn => 'Tab::Tourn');
+Tab::Housing->has_a(judge => 'Tab::Judge');
+Tab::Housing->has_a(school => 'Tab::School');
+Tab::Housing->has_a(account => 'Tab::Account');
+Tab::Housing->has_a(student => 'Tab::Student');
+
+__PACKAGE__->_register_datetimes( qw/timestamp/);
+__PACKAGE__->_register_datetimes( qw/requested/);
+__PACKAGE__->_register_dates( qw/night/);
+
