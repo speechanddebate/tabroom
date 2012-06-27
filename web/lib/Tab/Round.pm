74e6d322c14f58e42b2eafe2ef51dedb700e61e0
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-06-27 02:06:11 +0000

    more panel fun

--- web/lib/Tab/Round.pm
+++ web/lib/Tab/Round.pm
@@ -3,7 +3,7 @@ use base 'Tab::DBI';
 Tab::Round->table('round');
 Tab::Round->columns(Primary => qw/id/);
 Tab::Round->columns(Essential => qw/name label event type timeslot site pool/);
-Tab::Round->columns(Others => qw/published listed created completed timestamp tb_set/);
+Tab::Round->columns(Others => qw/published listed created completed blasted timestamp tb_set/);
 
 Tab::Round->has_a(event => 'Tab::Event');
 Tab::Round->has_a(site => 'Tab::Site');
@@ -13,6 +13,7 @@ Tab::Round->has_a(tb_set => 'Tab::TiebreakSet');
 
 Tab::Round->has_many(panels => 'Tab::Panel', 'round');
 
+__PACKAGE__->_register_dates( qw/blasted/);
 __PACKAGE__->_register_dates( qw/created/);
 __PACKAGE__->_register_dates( qw/completed/);
 __PACKAGE__->_register_dates( qw/timestamp/);
