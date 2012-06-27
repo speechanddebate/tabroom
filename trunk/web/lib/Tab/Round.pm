c1434f537507d5657a1f6c9973280a916c2d4131
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-06-27 01:43:14 +0000

    Pairings getting there

--- trunk/web/lib/Tab/Round.pm
+++ trunk/web/lib/Tab/Round.pm
@@ -3,7 +3,7 @@ use base 'Tab::DBI';
 Tab::Round->table('round');
 Tab::Round->columns(Primary => qw/id/);
 Tab::Round->columns(Essential => qw/name label event type timeslot site pool/);
-Tab::Round->columns(Others => qw/published listed created entryleted timestamp tb_set/);
+Tab::Round->columns(Others => qw/published listed created completed timestamp tb_set/);
 
 Tab::Round->has_a(event => 'Tab::Event');
 Tab::Round->has_a(site => 'Tab::Site');
@@ -14,7 +14,7 @@ Tab::Round->has_a(tb_set => 'Tab::TiebreakSet');
 Tab::Round->has_many(panels => 'Tab::Panel', 'round');
 
 __PACKAGE__->_register_dates( qw/created/);
-__PACKAGE__->_register_dates( qw/entryleted/);
+__PACKAGE__->_register_dates( qw/completed/);
 __PACKAGE__->_register_dates( qw/timestamp/);
 
 sub realname { 
