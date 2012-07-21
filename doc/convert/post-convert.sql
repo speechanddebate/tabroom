955e2ae26ca2c3133e5d1a5417d466850dd549fb
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-07-21 02:43:30 +0000

    Are we there yet?

--- doc/convert/post-convert.sql
+++ doc/convert/post-convert.sql
@@ -9,3 +9,6 @@ update ballot set side=1 where side="A";
 update ballot set side=2 where side="N";
 alter table ballot modify side tinyint;
 
+alter table tiebreak_set add type varchar(15);
+update tiebreak_set set type="Team" where type is null;
+
