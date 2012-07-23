d8b7422349e75bb0626a523a6264cae7af47f921
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-07-23 15:12:09 +0000

    Many fixes

--- web/lib/Tab/Pool.pm
+++ web/lib/Tab/Pool.pm
@@ -10,6 +10,7 @@ Tab::Pool->has_a(judge_group => "Tab::JudgeGroup");
 
 Tab::Pool->has_many(rounds => 'Tab::Round', 'pool');
 Tab::Pool->has_many(pool_judges => 'Tab::PoolJudge', 'pool');
+Tab::Pool->has_many(judges => [Tab::PoolJudge => 'judge']);
 
 __PACKAGE__->_register_datetimes( qw/timestamp/);
 
