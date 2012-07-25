de87d4411eeac75c3edb5eb475aea7f1fc25f35c
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-07-25 00:14:29 +0000

    Fuck!

--- trunk/web/lib/Tab/FollowEntry.pm
+++ trunk/web/lib/Tab/FollowEntry.pm
@@ -2,7 +2,7 @@ package Tab::FollowEntry;
 use base 'Tab::DBI';
 Tab::FollowEntry->table('follow_entry');
 Tab::FollowEntry->columns(Primary => qw/id/);
-Tab::FollowEntry->columns(Essential => qw/entry cell email domain follower/);
+Tab::FollowEntry->columns(Essential => qw/entry cell email domain follower timestamp/);
 Tab::FollowEntry->has_a(entry => 'Tab::Entry');
 Tab::FollowEntry->has_a(follower => 'Tab::Account');
 
--- trunk/web/lib/Tab/FollowJudge.pm
+++ trunk/web/lib/Tab/FollowJudge.pm
@@ -2,7 +2,6 @@ package Tab::FollowJudge;
 use base 'Tab::DBI';
 Tab::FollowJudge->table('follow_judge');
 Tab::FollowJudge->columns(Primary => qw/id/);
-Tab::FollowJudge->columns(Essential => qw/judge follower/);
+Tab::FollowJudge->columns(Essential => qw/judge cell email domain timestamp follower/);
 Tab::FollowJudge->has_a(judge => 'Tab::Judge');
 Tab::FollowJudge->has_a(follower => 'Tab::Account');
-
