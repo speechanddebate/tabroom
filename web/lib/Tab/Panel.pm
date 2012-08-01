f6acd9c6a084cb4b46e7c8bc3fc69c5b31317802
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-08-01 18:50:44 +0000

    Many Michigan changes

--- web/lib/Tab/Panel.pm
+++ web/lib/Tab/Panel.pm
@@ -3,7 +3,7 @@ use base 'Tab::DBI';
 Tab::Panel->table('panel');
 Tab::Panel->columns(Primary => qw/id/);
 Tab::Panel->columns(Essential => qw/letter round room flight bye timestamp/);
-Tab::Panel->columns(TEMP => qw/judge/);
+Tab::Panel->columns(TEMP => qw/judge audit/);
 
 Tab::Panel->has_a(room => 'Tab::Room');
 Tab::Panel->has_a(round => 'Tab::Round');
