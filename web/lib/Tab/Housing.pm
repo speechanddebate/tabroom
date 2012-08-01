26634da6bc56014701a6f6d27760197ed347bede
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-08-01 01:48:51 +0000

    Many live changes

--- web/lib/Tab/Housing.pm
+++ web/lib/Tab/Housing.pm
@@ -2,7 +2,7 @@ package Tab::Housing;
 use base 'Tab::DBI';
 Tab::Housing->table('housing');
 Tab::Housing->columns(Primary => "id");
-Tab::Housing->columns(Essential => qw/tourn student judge type waitlist night requested account school timestamp/);
+Tab::Housing->columns(Essential => qw/tourn student tba judge type waitlist night requested account school timestamp/);
 
 Tab::Housing->has_a(tourn => 'Tab::Tourn');
 Tab::Housing->has_a(judge => 'Tab::Judge');
