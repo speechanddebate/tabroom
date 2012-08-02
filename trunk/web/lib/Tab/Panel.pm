4ec1eb993a6699358f5ab7c25d6354ef50752da3
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-08-02 14:24:18 +0000

    Yes

--- trunk/web/lib/Tab/Panel.pm
+++ trunk/web/lib/Tab/Panel.pm
@@ -2,7 +2,7 @@ package Tab::Panel;
 use base 'Tab::DBI';
 Tab::Panel->table('panel');
 Tab::Panel->columns(Primary => qw/id/);
-Tab::Panel->columns(Essential => qw/letter round room flight bye timestamp/);
+Tab::Panel->columns(Essential => qw/letter round room flight bye started timestamp/);
 Tab::Panel->columns(TEMP => qw/judge audit/);
 
 Tab::Panel->has_a(room => 'Tab::Room');
@@ -10,7 +10,7 @@ Tab::Panel->has_a(round => 'Tab::Round');
 
 Tab::Panel->has_many(ballots => 'Tab::Ballot', 'panel');
 
-__PACKAGE__->_register_dates( qw/timestamp/);
+__PACKAGE__->_register_dates( qw/started timestamp/);
 
 sub entries {
     my $self = shift;
