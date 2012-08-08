266ab8c2577d0403d73671c2700a0b176da31425
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-08-08 02:41:04 +0000

    Slowdowns averted with indexes on judge(account)

--- doc/convert/post-convert.sql
+++ doc/convert/post-convert.sql
@@ -1,3 +1,12 @@
 
 alter table panel add started datetime;
 
+alter table judge_hire add rounds_accepted int;
+
+create index account on judge(account);
+create index account on student(account);
+create index account on chapter_judge(account);
+create index chapter_judge on judge(chapter_judge);
+create index chapter on chapter_judge(chapter);
+
+
