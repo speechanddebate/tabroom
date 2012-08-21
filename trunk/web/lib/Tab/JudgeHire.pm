2d78bdaf80ba35dd32f695f0c3d7c344f926031f
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-08-21 01:02:56 +0000

    Judge hiring system; maybe done, requires testing

--- trunk/web/lib/Tab/JudgeHire.pm
+++ trunk/web/lib/Tab/JudgeHire.pm
@@ -5,6 +5,7 @@ Tab::JudgeHire->columns(Primary => qw/id/);
 Tab::JudgeHire->columns(Essential => qw/judge tourn judge_group school rounds rounds_accepted covers accepted request_made timestamp /);
 
 Tab::JudgeHire->has_a(tourn => "Tab::Tourn");
+Tab::JudgeHire->has_a(judge => "Tab::Judge");
 Tab::JudgeHire->has_a(school => "Tab::School");
 Tab::JudgeHire->has_a(judge_group => "Tab::JudgeGroup");
 
