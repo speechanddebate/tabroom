26ea808eb4aad783b83b82e1c6eecd771b488c00
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-08-31 18:14:53 +0000

    Ordinal preferences

--- trunk/web/lib/Tab/Rating.pm
+++ trunk/web/lib/Tab/Rating.pm
@@ -2,7 +2,8 @@ package Tab::Rating;
 use base 'Tab::DBI';
 Tab::Rating->table('rating');
 Tab::Rating->columns(Primary => qw/id/);
-Tab::Rating->columns(Essential => qw/tourn school entry type rating_tier entered judge rating_subset ordinal timestamp /);
+Tab::Rating->columns(Essential => qw/tourn school entry type rating_tier entered
+									judge rating_subset ordinal percentile timestamp /);
 
 Tab::Rating->has_a(tourn => 'Tab::Tourn');
 Tab::Rating->has_a(school => 'Tab::School');
