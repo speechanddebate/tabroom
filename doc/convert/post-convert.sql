822665c65c853be869d837e7b411a9b16d12d63b
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-09-13 04:20:13 +0000

    Pools and judges.  Judge paneling.

--- doc/convert/post-convert.sql
+++ doc/convert/post-convert.sql
@@ -4,4 +4,6 @@ alter table chapter_judge add diet varchar(31);
 
 create index tourn on school(tourn);
 
+alter table pool add standby_timeslot int;
+alter table pool add tourn int;
 
--- web/lib/Tab/Pool.pm
+++ web/lib/Tab/Pool.pm
@@ -2,10 +2,13 @@ package Tab::Pool;
 use base 'Tab::DBI';
 Tab::Pool->table('pool');
 Tab::Pool->columns(Primary => qw/id/);
-Tab::Pool->columns(Essential => qw/name judge_group standby timestamp/);
+Tab::Pool->columns(Essential => qw/name tourn judge_group standby standby_timeslot timestamp/);
 Tab::Pool->columns(Others => qw/site publish registrant burden event_based/);
 
+Tab::Pool->has_a(standby_timeslot => 'Tab::Timeslot');
+
 Tab::Pool->has_a(site => 'Tab::Site');
+Tab::Pool->has_a(tourn => 'Tab::Tourn');
 Tab::Pool->has_a(judge_group => "Tab::JudgeGroup");
 
 Tab::Pool->has_many(rounds => 'Tab::Round', 'pool');
