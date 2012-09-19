1f4c8d0ce47642de40913fc2453b6776a3e3402e
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-09-19 18:00:51 +0000

    Ordering engine appears to be a go

--- web/lib/Tab/TiebreakSet.pm
+++ web/lib/Tab/TiebreakSet.pm
@@ -1,7 +1,7 @@
 package Tab::TiebreakSet;
 use base 'Tab::DBI';
 Tab::TiebreakSet->table('tiebreak_set');
-Tab::TiebreakSet->columns(All => qw/id tourn name timestamp type/);
+Tab::TiebreakSet->columns(All => qw/id tourn name timestamp type elim/);
 Tab::TiebreakSet->has_a(tourn => 'Tab::Tourn');
 Tab::TiebreakSet->has_many(tiebreaks => 'Tab::Tiebreak', 'tb_set');
 Tab::TiebreakSet->has_many(rounds => 'Tab::Tiebreak', 'tb_set');
