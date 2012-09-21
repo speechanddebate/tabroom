c4ddc4c6c409571c45ede24f0d2bdfe2d976ff10
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-09-21 02:19:49 +0000

    Results are done

--- web/lib/Tab/BallotValue.pm
+++ web/lib/Tab/BallotValue.pm
@@ -3,6 +3,7 @@ use base 'Tab::DBI';
 Tab::BallotValue->table('ballot_value');
 Tab::BallotValue->columns(Primary => qw/id/);
 Tab::BallotValue->columns(Essential => qw/ballot tag student tiebreak value timestamp content cat_id/);
+Tab::BallotValue->columns(TEMP => qw/entryid/);
 
 Tab::BallotValue->has_a(ballot => 'Tab::Ballot');
 Tab::BallotValue->has_a(student => 'Tab::Student');
