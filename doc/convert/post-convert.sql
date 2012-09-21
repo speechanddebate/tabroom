7f03257cf4ba5302b1ed21b277fa880f1a78c6dc
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-09-21 18:01:22 +0000

    Adding sweepstakes rules

--- doc/convert/post-convert.sql
+++ doc/convert/post-convert.sql
@@ -1,9 +1,39 @@
 
-alter table student add diet varchar(31);
-alter table chapter_judge add diet varchar(31);
+create table sweep_set (
+	id int auto_increment primary key,
+	tourn int,
+	name varchar(31),
+	timestamp timestamp
+);
 
-create index tourn on school(tourn);
+create table sweep_event ( 
+	id int auto_increment primary key,
+	sweep_set int,
+	event int,
+	timestamp timestamp
+);
 
-alter table pool add standby_timeslot int;
-alter table pool add tourn int;
+create table sweep_rule ( 
+	id int auto_increment primary key,
+	sweep_set int,
+	tag varchar(31),
+	value varchar(63),
+	timestamp timestamp
+);
+
+create index sweep_set on sweep_rule(sweep_set);
+create index sweep_set on sweep_event(sweep_set);
+create index event on sweep_event(event);
+create index tourn on sweep_set(tourn);
+
+create index entry on rating(entry);
+create index judge on rating(judge);
+
+#settings:
+#sweep_max_events
+#sweep_max_sets
+#sweep_max_per_event
+#sweep_max_overall
+#sweep_max_per_set
+#sweep_wildcards
 
