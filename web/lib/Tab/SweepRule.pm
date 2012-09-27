f935c50fbd74d980b1377fcda807b2904b89f722
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-09-27 06:26:49 +0000

    Yay

--- web/lib/Tab/SweepRule.pm
+++ web/lib/Tab/SweepRule.pm
@@ -1,7 +1,7 @@
 package Tab::SweepRule;
 use base 'Tab::DBI';
 Tab::SweepRule->table('sweep_rule');
-Tab::SweepRule->columns(All => qw/id sweep_set tag value timestamp/);
+Tab::SweepRule->columns(All => qw/id sweep_set tag value place timestamp/);
 Tab::SweepRule->has_a(sweep_set => 'Tab::SweepSet');
 __PACKAGE__->_register_datetimes( qw/timestamp/);
 
--- web/lib/Tab/SweepSet.pm
+++ web/lib/Tab/SweepSet.pm
@@ -3,7 +3,51 @@ use base 'Tab::DBI';
 Tab::SweepSet->table('sweep_set');
 Tab::SweepSet->columns(All => qw/id tourn name timestamp/);
 Tab::SweepSet->has_a(tourn => 'Tab::Tourn');
-Tab::SweepSet->has_many(sweep_rules => 'Tab::SweepRule', 'sweep_set');
-Tab::SweepSet->has_many(sweep_events => 'Tab::SweepEvent', 'sweep_set');
+Tab::SweepSet->has_many(rules => 'Tab::SweepRule', 'sweep_set');
+Tab::SweepSet->has_many(sweep_events => 'Tab::SweepEvent', 'sweep_event');
 Tab::SweepSet->has_many(events => [Tab::SweepEvent => 'event']);
 __PACKAGE__->_register_datetimes( qw/timestamp/);
+
+sub rule {
+
+	my ($self, $tag, $value) = @_;
+
+	my @existing = Tab::SweepRule->search(  
+		event => $self->id,
+		tag => $tag
+	);
+
+    if (defined $value) {
+
+		if (@existing) {
+
+			my $exists = shift @existing;
+			if ($value eq "delete" || $value eq "" || $value eq 0) { 
+				$exists->delete;
+			} else { 
+				$exists->value($value);
+				$exists->update;
+			}
+			foreach my $other (@existing) { 
+				$other->delete;
+			}
+			return;
+
+		} elsif ($value ne "delete" && $value && $value ne "0") {
+
+			my $exists = Tab::SweepRule->create({
+				event => $self->id,
+				tag => $tag,
+				value => $value,
+			});
+
+			$exists->update;
+		}
+
+	} else {
+		return unless @existing;
+		my $setting = shift @existing;
+		return $setting->value;
+	}
+
+}
