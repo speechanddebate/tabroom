130f833aeeb1aefa074ac3af9e8d3403dddad3e6
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-10-03 22:32:15 +0000

    Yes

--- web/lib/Tab/EventDouble.pm
+++ web/lib/Tab/EventDouble.pm
@@ -2,6 +2,7 @@ package Tab::EventDouble;
 use base 'Tab::DBI';
 Tab::EventDouble->table('event_double');
 Tab::EventDouble->columns(Primary =>qw/id/);
-Tab::EventDouble->columns(Essential => qw/name tourn setting timestamp min max/);
+Tab::EventDouble->columns(Essential => qw/name tourn setting timestamp min max exclude/);
 Tab::EventDouble->has_a(tourn => 'Tab::Tourn');
+Tab::EventDouble->has_a(exclude => 'Tab::EventDouble');
 Tab::EventDouble->has_many(events => "Tab::Event", 'event_double');
