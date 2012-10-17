6860f6ae85fc6ec83778a7c9ff29a4b97916c1fc
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-10-17 17:02:45 +0000

    Yes

--- trunk/web/lib/Tab/Session.pm
+++ trunk/web/lib/Tab/Session.pm
@@ -2,10 +2,11 @@ package Tab::Session;
 use base 'Tab::DBI';
 Tab::Session->table('session');
 Tab::Session->columns(All => qw/id account authkey userkey timestamp ip limited entry_only circuit tourn school event su/);
-
 Tab::Session->has_a(account => 'Tab::Account');
 Tab::Session->has_a(su => 'Tab::Account');
 Tab::Session->has_a(circuit => 'Tab::Circuit');
 Tab::Session->has_a(school => 'Tab::School');
 Tab::Session->has_a(tourn => 'Tab::Tourn');
+Tab::Session->has_a(event => 'Tab::Event');
+
 __PACKAGE__->_register_datetimes( qw/timestamp/);
