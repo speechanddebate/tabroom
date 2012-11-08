a208107bfcacd82aa497e6a0b837b2108475e822
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-11-08 04:36:44 +0000

    Student interface begins

---
+++ trunk/doc/schema-updates.sql
@@ -0,0 +1,26 @@
+
+#create table sweep_set (
+#	id int auto_increment primary key,
+#	tourn int,
+#	name varchar(31),
+#	timestamp timestamp
+#);
+
+#create index sweep_set on sweep_rule(sweep_set);
+#alter table judge add tab_rating int;
+
+create table account_conflict ( 
+	id int auto_increment primary key,
+	account int not null,
+	conflict int not null default 0,
+	chapter int not null default 0,
+	added_by int not null default 0,
+	timestamp timestamp
+);
+
+create index chapter on account_conflict(chapter);
+create index account on account_conflict(account);
+create index conflict on account_conflict(conflict);
+create index added_by on account_conflict(added_by);
+
+
