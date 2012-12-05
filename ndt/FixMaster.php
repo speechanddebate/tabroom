db41ad0d2307db9fb0cd91a35e31f4fbbf593eb4
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2012-12-05 00:28:09 +0000

    yes

---
+++ ndt/FixMaster.php
@@ -0,0 +1,9 @@
+<a href="FixerElimBye.php">Elim byes</a></br>
+
+<a href="FixerMarkDivLevels.php">Mark division levels</a></br>
+
+<a href="FixerResults.php">Add a ballot_value to an elim ballot that doesn't have one</a></br>
+
+<a href="FixerUglyBallotEditor.php">Change the ballot_value on a ballot</a></br>
+
+<a href="FixerUglyResultDisplay.php">Flip sides or decisions/run custom query</a></br>
---
+++ ndt/FixerUglyBallotEditor.php
@@ -0,0 +1,55 @@
+<?php
+require 'scripts/databaseconnect.php';
+
+$ballot=$_GET['ballot'];
+$query="SELECT * FROM ballot_value WHERE ballot=".$ballot;
+
+$tourn=mysql_query($query);
+$entryNum = mysql_num_rows($tourn);
+?>
+UGLY BALLOT EDITOR</br>
+Receives a ballotID and shows all values associated with it</br>
+       <table id="contact" class="hovertable sortable" border="2" cellspacing="2" cellpadding="2">
+       <thead>
+	<tr class="yellowrow">
+		<th class="smallish">ID</th>
+		<th class="smallish">ballot</th>
+		<th class="smallish">tag</th>
+		<th class="smallish">student</th>
+		<th class="smallish">tiebreak</th>
+		<th class="smallish">value</th>
+		<th class="smallish">content</th>
+	</tr>
+	</thead>
+
+    <tbody id="myTbodytourneytot">
+
+
+<?php
+for ($i=0; $i <= $entryNum-1; $i++)
+{
+echo "<tr>";
+echo "<td>".mysql_result($tourn,$i,"id")."</td>";
+echo "<td>".mysql_result($tourn,$i,"ballot")."</td>";
+echo "<td>".mysql_result($tourn,$i,"tag")."</td>";
+echo "<td>".mysql_result($tourn,$i,"student")."</td>";
+echo "<td>".mysql_result($tourn,$i,"tiebreak")."</td>";
+echo "<td>".mysql_result($tourn,$i,"value")."</td>";
+echo "<td>".mysql_result($tourn,$i,"content")."</td>";
+echo "</tr>";
+}
+
+?>
+
+    </tbody></table>
+
+<form action="DoSQLUpdate.php" method="post">
+ <p>Enter the SQL query you wish to run: <input type="text" size="250" name="querystring" value="Update ballot_value set value=1 where ballot_value.id=1156828"></input></p>
+ <p><input type="submit" /></p>
+</form>
+
+<?php
+
+mysql_close();
+
+?>
