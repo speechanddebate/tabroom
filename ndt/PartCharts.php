885dfef39eed15b65d0cc0fac3af959df09bcd99
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2013-01-03 14:01:56 +0000

    Changes

--- ndt/PartCharts.php
+++ ndt/PartCharts.php
@@ -4,9 +4,13 @@ require 'scripts/tabroomtemplate.html';
 require 'scripts/databaseconnect.php';
 
 //Load all students
-$query="SELECT *, student.id as student_id FROM student, chapter_circuit where student.chapter=chapter_circuit.chapter and chapter_circuit.circuit=43 ORDER BY student.id";
+$query="SELECT *, student.id as student_id, student.chapter as student_chapter FROM student, chapter_circuit where student.chapter=chapter_circuit.chapter and chapter_circuit.circuit=43 and retired=FALSE ORDER BY student.id";
 $student=mysql_query($query);
 $studentnum = mysql_num_rows($student);
+//echo "LOAD<BR>";
+//while ($row = mysql_fetch_array($student, MYSQL_BOTH)) {if ($row['student_chapter']==6439) {echo $row['student_id']." ".$row['first']." ".$row['last']." ".$row['student_chapter']."<br>";}}
+//echo "END LOAD<BR>";
+
 
 //Mark participation
 $nrounds=array(); $ntourn=array(); $chapter=array();
@@ -25,7 +29,7 @@ while ($row = mysql_fetch_array($student, MYSQL_BOTH)) {countstuff($row['student
     <tbody id="myTbodytotals">
 
 <?php
-echo "<tr><td>Total student accounts</td><td>".$studentnum."</td></tr>";
+echo "<tr><td>Total non-retired student records</td><td>".$studentnum."</td></tr>";
 echo "<tr><td>Students with 1+ round</td><td>".counter(1, $nrounds, -99, $chapter)."</td></tr>";
 echo "<tr><td>Students with 5+ round</td><td>".counter(5, $nrounds, -99, $chapter)."</td></tr>";
 echo "<tr><td>Students with 10+ round</td><td>".counter(10, $nrounds, -99, $chapter)."</td></tr>";
@@ -40,6 +44,74 @@ echo "<tr><td>Students attending 8+ tournaments</td><td>".counter(8, $ntourn, -9
 echo "<tr><td>Students attending 10+ tournaments</td><td>".counter(10, $ntourn, -99, $chapter)."</td></tr>";
 echo "<tr><td>Students attending 12+ tournaments</td><td>".counter(12, $ntourn, -99, $chapter)."</td></tr>";
 
+//CLOSE UP AND SWITCH TO SCHOOLS	
+?>
+	</tbody>
+       </table>
+<br/>
+<h2>Participation by School</h2>
+       <table id="sortme" class="hovertable sortable" border="2" cellspacing="2" cellpadding="2">
+       <thead>
+	<tr class="yellowrow">
+		<th class="smallish">School</th>
+              <th class="smallish">Competitors</th>
+              <th class="smallish">Comp. at 1+ tourneys</th>
+              <th class="smallish">Comp. at 3+ tourneys</th>
+              <th class="smallish">Comp. at 5+ tourneys</th>
+              <th class="smallish">Comp. at 10+ tourneys</th>
+	</tr>
+	</thead>
+    <tbody id="myTbodytotals">
+
+<?php
+//Load all SCHOOLS
+$query="SELECT *, chapter.name as chapter_name, chapter.id as chapter_id FROM chapter_circuit, chapter where chapter_circuit.circuit=43 and chapter.id=chapter_circuit.chapter order by chapter.name";
+$school=mysql_query($query);
+while ($row = mysql_fetch_array($school, MYSQL_BOTH)) 
+ { 
+  echo "<tr>";
+  echo "<td>".$row['chapter_name']."</td>";
+  echo "<td>".countcompbyschool($row['chapter_id'], $student)."</td>";
+  echo "<td>".counter(1, $ntourn, $row['chapter_id'], $chapter)."</td>";
+  echo "<td>".counter(3, $ntourn, $row['chapter_id'], $chapter)."</td>";
+  echo "<td>".counter(5, $ntourn, $row['chapter_id'], $chapter)."</td>";
+  echo "<td>".counter(10, $ntourn, $row['chapter_id'], $chapter)."</td>";
+  echo "</tr>";
+ }
+?>
+	</tbody>
+       </table>
+
+<br/>
+<h2>Participation by Tournament</h2>
+       <table id="sortme" class="hovertable sortable" border="2" cellspacing="2" cellpadding="2">
+       <thead>
+	<tr class="yellowrow">
+		<th class="smallish">Tournament</th>
+              <th class="smallish">Open Entries</th>
+              <th class="smallish">JV Entries</th>
+              <th class="smallish">Novice Entries</th>
+              <th class="smallish">Total Entries</th>
+	</tr>
+	</thead>
+    <tbody id="myTourney">
+
+<?php
+//Load all TOURNEYS
+$query="SELECT *, tourn.name as tourn_name, tourn.id as tourn_id FROM tourn, tourn_circuit where tourn_circuit.circuit=43 and tourn.id=tourn_circuit.tourn order by tourn.name";
+$tourns=mysql_query($query);
+while ($row = mysql_fetch_array($tourns, MYSQL_BOTH)) 
+ { 
+  echo "<tr>";
+  echo "<td>".$row['tourn_name']."</td>";
+  $open=getnentries($row['tourn_id'],'open'); echo "<td>".$open."</td>";
+  $jv=getnentries($row['tourn_id'],'jv'); echo "<td>".$jv."</td>";
+  $novice=getnentries($row['tourn_id'],'novice'); echo "<td>".$novice."</td>";
+  $total=$open+$jv+$novice;
+  echo "<td>".$total."</td>";
+  echo "</tr>";
+
+ }
 
 ?>
 	</tbody>
@@ -54,13 +126,33 @@ $time = $time_end - $time_start;
 echo "Total load time is $time seconds\n";
 require 'scripts/tabroomfooter.html';
 
+function getnentries($tourn_id, $division)
+{
+$query="SELECT * FROM entry, event, event_setting WHERE event.tourn=".$tourn_id." and entry.event=event.id and event.type='policy' and event_setting.event=event.id and event_setting.tag='Level' and event_setting.value='".$division."'";
+$results=mysql_query($query);
+$nentries = mysql_num_rows($results);
+return $nentries;
+}
+
 function counter($target, $array, $targetchapter, $chapter)
 {
  $ctr=0;
  foreach ($array as $index_name => $value) 
  {
   if ($value>=$target and $targetchapter==-99) {$ctr++;}
+  if ($value>=$target and $targetchapter==$chapter[$index_name]) {$ctr++;}
  }
+ return $ctr;
+}
+
+function countcompbyschool($chapter, $student)
+{
+ $ctr=0;
+ mysql_data_seek($student,0);
+ while ($row = mysql_fetch_array($student, MYSQL_BOTH))
+  {if ($row['student_chapter']==$chapter) {$ctr++;}
+   //if ($row['student_chapter']==$chapter and $chapter==6306) {echo $row['first']." ".$row['last']." ".$row['student_chapter']."<br>";}
+  }
  return $ctr;
 }
 
--- ndt/ShowTableSchema.php
+++ ndt/ShowTableSchema.php
@@ -6,7 +6,7 @@ $database="itab";
 mysql_connect("localhost",$username,$password);
 @mysql_select_db($database) or die( "Unable to select database<br>");
 
-$result = mysql_query("SHOW COLUMNS FROM round");
+$result = mysql_query("SHOW COLUMNS FROM event_setting");
 if (!$result) {
     echo 'Could not run query: ' . mysql_error();
     exit;
@@ -19,7 +19,7 @@ if (mysql_num_rows($result) > 0) {
 
 echo "<br>";
 
-$query="SELECT distinct tag from ballot_value order by tag";
+$query="SELECT distinct tag from event_setting order by tag";
 //$query="SELECT * from tiebreak_set where id>5800";
 $tourn=mysql_query($query);
 $entryNum = mysql_num_rows($tourn);
@@ -27,7 +27,7 @@ $entryNum = mysql_num_rows($tourn);
 for ($i=0; $i <= $entryNum-1; $i++)
 {
 echo mysql_result($tourn,$i,"tag")."<br>";
-//echo mysql_result($tourn,$i,"id")." ".mysql_result($tourn,$i,"tag")." ".mysql_result($tourn,$i,"tiebreak")."<br>";
+//echo mysql_result($tourn,$i,"id")." ".mysql_result($tourn,$i,"type")." ".mysql_result($tourn,$i,"tiebreak")."<br>";
 
 }
 
@@ -39,6 +39,12 @@ for ($i=0; $i <= $entryNum-1; $i++)
 {
 echo mysql_result($tourn,$i,"table_name")."<br>";
 }
+
+echo "<br>";
+
+$sql = mysql_query("SELECT * FROM student WHERE last = 'Lurie-Spicer'");
+$assoc = mysql_fetch_assoc($sql);
+var_dump($assoc);
 
 mysql_close();
 ?>
