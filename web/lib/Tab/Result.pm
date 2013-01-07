7895babef429b43c691b5f920aebe37ef943aae6
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2013-01-07 16:11:43 +0000

    Bracketology

--- web/lib/Tab/Result.pm
+++ web/lib/Tab/Result.pm
@@ -2,9 +2,10 @@ package Tab::Result;
 use base 'Tab::DBI';
 Tab::Result->table('result');
 Tab::Result->columns(Primary => qw/id/);
-Tab::Result->columns(Essential => qw/result_set entry student school timestamp/);
+Tab::Result->columns(Essential => qw/result_set entry student school round timestamp/);
 
 Tab::Result->has_a(entry => 'Tab::Entry');
+Tab::Result->has_a(round => 'Tab::Round');
 Tab::Result->has_a(student => 'Tab::Student');
 Tab::Result->has_a(result_set => 'Tab::ResultSet');
 
--- web/lib/Tab/ResultSet.pm
+++ web/lib/Tab/ResultSet.pm
@@ -2,7 +2,7 @@ package Tab::ResultSet;
 use base 'Tab::DBI';
 Tab::ResultSet->table('result_set');
 Tab::ResultSet->columns(Primary => qw/id/);
-Tab::ResultSet->columns(Essential => qw/id tourn event label generated timestamp/);
+Tab::ResultSet->columns(Essential => qw/id tourn event bracket label generated timestamp/);
 Tab::ResultSet->has_a(tourn => 'Tab::Tourn');
 Tab::ResultSet->has_a(event => 'Tab::Event');
 Tab::ResultSet->has_many(results => 'Tab::Result', 'result_set');
