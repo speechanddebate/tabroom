d588e4da7cf4499a14692de819449eb41f8fa079
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2013-01-09 00:40:17 +0000

    Yay

--- trunk/web/lib/Tab/TournChange.pm
+++ trunk/web/lib/Tab/TournChange.pm
@@ -5,6 +5,7 @@ Tab::TournChange->columns(Primary => qw/id/);
 Tab::TournChange->columns(Essential => qw/type tourn school account/);
 Tab::TournChange->columns(Others => qw/judge entry event new_panel old_panel fine text timestamp/);
 
+Tab::TournChange->has_a(account => "Tab::Account");
 Tab::TournChange->has_a(tourn => "Tab::Tourn");
 Tab::TournChange->has_a(event => "Tab::Event");
 Tab::TournChange->has_a(entry => "Tab::Entry");
