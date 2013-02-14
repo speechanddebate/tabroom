1e50e896788009a0eb43546df0b5f5970a006ca5
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2013-02-14 17:38:55 +0000

    Prime fun

--- web/lib/Tab/Region.pm
+++ web/lib/Tab/Region.pm
@@ -10,40 +10,5 @@ Tab::Region->has_a(circuit => 'Tab::Circuit');
 Tab::Region->has_many(schools => 'Tab::School', 'region');
 Tab::Region->has_many(fines => 'Tab::RegionFine', 'region');
 Tab::Region->has_many(region_admins => 'Tab::RegionAdmin', 'region');
-
-sub admins {
-    my $self = shift;
-    return Tab::Account->search_by_region_admin($self->id);
-}
-
-sub events {
-	my ($self, $tourn) = @_;
-	return Tab::Event->search_by_region_and_tourn($self->id, $tourn->id);
-}
-
-sub entries {
-    my ($self,$tourn) = @_;
-	return Tab::Entry->search_by_region_and_tourn($self->id, $tourn->id);	
-}
-
-sub judges {
-    my ($self,$tourn) = @_;
-	return Tab::Judge->search_by_region($self->id, $tourn->id);
-}
-
-sub event_entries {
-	my ($self, $event) = @_;
-	my @entries = sort {$a->id <=> $b->id} Tab::Entry->search_by_region_and_event($self->id, $event->id);
-	return @entries;
-}
-
-sub group_entries {
-    my ($self,$group) = @_;
-	return Tab::Entry->search_by_region_and_group($self->id, $group->id);	
-}
-
-sub chapters {
-    my $self = shift;
-    return Tab::Chapter->search_by_region($self->id);
-}
+Tab::Region->has_many(admins => [ Tab::RegionAdmin => 'account']);
 
