6fe83ac1af51f9efd2367008ac08c7d161f0bf6a
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2013-02-28 19:06:48 +0000

    bug fixed

--- trunk/ndt/ShowTableSchema.php
+++ trunk/ndt/ShowTableSchema.php
@@ -6,7 +6,7 @@ $database="itab";
 mysql_connect("localhost",$username,$password);
 @mysql_select_db($database) or die( "Unable to select database<br>");
 
-$result = mysql_query("SHOW COLUMNS FROM entry_student");
+$result = mysql_query("SHOW COLUMNS FROM student");
 if (!$result) {
     echo 'Could not run query: ' . mysql_error();
     exit;
