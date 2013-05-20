191116d95fde341e31a7e44ba6050e1aaac23ba1
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2013-05-20 19:02:43 +0000

    NCFL Congress done; other changes to come through testing.

--- trunk/web/lib/Tab/BallotValue.pm
+++ trunk/web/lib/Tab/BallotValue.pm
@@ -4,7 +4,7 @@ Tab::BallotValue->table('ballot_value');
 Tab::BallotValue->columns(Primary => qw/id/);
 Tab::BallotValue->columns(Essential => qw/ballot tag student value/);
 Tab::BallotValue->columns(Others => qw/timestamp content cat_id/);
-Tab::BallotValue->columns(TEMP => qw/panelid entryid roundtype roundid studentid judgeid bye ballotid/);
+Tab::BallotValue->columns(TEMP => qw/panelid entryid roundtype roundid studentid judgeid bye ballotid chair/);
 
 Tab::BallotValue->has_a(ballot => 'Tab::Ballot');
 Tab::BallotValue->has_a(student => 'Tab::Student');
