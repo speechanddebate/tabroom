fb7b079557b57b3a41bae0b2efb9fb093dd56659
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2013-05-21 23:58:16 +0000

    Huge speed improvements

--- trunk/web/lib/Tab/ResultValue.pm
+++ trunk/web/lib/Tab/ResultValue.pm
@@ -2,7 +2,8 @@ package Tab::ResultValue;
 use base 'Tab::DBI';
 Tab::ResultValue->table('result_value');
 Tab::ResultValue->columns(Primary => qw/id/);
-Tab::ResultValue->columns(Essential => qw/result tag value priority sort_desc no_sort timestamp /);
+Tab::ResultValue->columns(Essential => qw/result tag value priority sort_desc no_sort/);
+Tab::ResultValue->columns(Others => qw/long_tag timestamp /);
 Tab::ResultValue->has_a(result => 'Tab::Result');
 
 __PACKAGE__->_register_dates( qw/timestamp/);
