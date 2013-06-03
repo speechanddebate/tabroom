e0f606f0cc9c4355eb7a00d878afad7a456ef71f
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2013-06-03 22:59:27 +0000

    Yes

--- doc/convert/post-convert.sql
+++ doc/convert/post-convert.sql
@@ -16,5 +16,22 @@ create table sweep_include (
 	timestamp timestamp
 );
 
+create table tiebreak_setting ( 
+	id int auto_increment primary key,
+	tiebreak_set int,
+	tag varchar(31),
+	value varchar(127),
+	timestamp timestamp
+);
+
+create index tiebreak_set on tiebreak_setting(tiebreak_set);
+create index event on event_setting(event);
+create index judge on judge_setting(judge);
+create index judge_group on judge_group_setting(judge_group);
+create index tourn on tourn_setting(tourn);
+
+alter table result add rank int;
+alter table result add percentile int; 
+
 
 
