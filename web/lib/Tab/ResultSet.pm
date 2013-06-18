fa4b20e2a03e7ffbd453f5fbe2ed84f71be6dbec
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2013-06-18 14:05:21 +0000

    Things have changed

--- web/lib/Tab/ResultSet.pm
+++ web/lib/Tab/ResultSet.pm
@@ -4,7 +4,7 @@ Tab::ResultSet->table('result_set');
 Tab::ResultSet->columns(Primary => qw/id/);
 Tab::ResultSet->columns(Essential => qw/id tourn event bracket label generated timestamp/);
 Tab::ResultSet->has_a(tourn => 'Tab::Tourn');
-Tab::ResultSet->has_a(event => 'Tab::Event');
+Tab::ResultSet->has_a(event => 'Tab::Event' => { order_by => 'rank, id'} );
 Tab::ResultSet->has_many(results => 'Tab::Result', 'result_set');
 
 __PACKAGE__->_register_datetimes( qw/timestamp generated/);
