04535c8f6ba7d0670f2e5716398276e2e2407630
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2013-07-01 00:33:17 +0000

    WUDC breaks to elims

--- web/lib/Tab/Rating.pm
+++ web/lib/Tab/Rating.pm
@@ -2,8 +2,8 @@ package Tab::Rating;
 use base 'Tab::DBI';
 Tab::Rating->table('rating');
 Tab::Rating->columns(Primary => qw/id/);
-Tab::Rating->columns(Essential => qw/tourn school entry type rating_tier entered
-									judge rating_subset ordinal percentile timestamp /);
+Tab::Rating->columns(Essential => qw/tourn school entry type rating_tier judge rating_subset ordinal percentile /);
+Tab::Rating->columns(Others => qw/entered timestamp/);
 
 Tab::Rating->has_a(tourn => 'Tab::Tourn');
 Tab::Rating->has_a(school => 'Tab::School');
@@ -13,6 +13,5 @@ Tab::Rating->has_a(judge => 'Tab::Judge');
 Tab::Rating->has_a(rating_tier => 'Tab::RatingTier');
 Tab::Rating->has_a(rating_subset => 'Tab::RatingSubset');
 
-__PACKAGE__->_register_datetimes( qw/entered/ );
-__PACKAGE__->_register_datetimes( qw/timestamp/);
+__PACKAGE__->_register_datetimes( qw/entered timestamp/ );
 
