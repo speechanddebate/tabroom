a1625f3451fa0d1e54bd2c5e4b482ff2602fc549
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2013-11-20 00:36:11 +0000

    Assigned bye

--- doc/convert/post-convert.sql
+++ doc/convert/post-convert.sql
@@ -4,4 +4,8 @@ alter table student add school_sid varchar(63);
 alter table student add race varchar(63);
 alter table round add note varchar(255);
 
+alter table room add ada bool not null default 0;
+alter table entry add flight int not null default 0;
+
+
 
