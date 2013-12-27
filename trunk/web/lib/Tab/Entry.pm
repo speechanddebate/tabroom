c7d1fa3ff8192ca83bdb042cd595f700b887f206
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2013-12-27 20:02:55 +0000

    Many updates and wins

--- trunk/web/lib/Tab/Entry.pm
+++ trunk/web/lib/Tab/Entry.pm
@@ -4,7 +4,7 @@ Tab::Entry->table('entry');
 Tab::Entry->columns(Primary => qw/id/);
 Tab::Entry->columns(Essential => qw/code dropped name school tourn event waitlist dq unconfirmed/);
 Tab::Entry->columns(Others => qw/seed bid title ada drop_time reg_time timestamp drop_by reg_by off_waitlist tba self_reg_by sweeps placement pair_seed cat_id flight/);
-Tab::Entry->columns(TEMP => qw/panelid speaks side ballot othername schname regname regcode region pullup bracketseed/);
+Tab::Entry->columns(TEMP => qw/panelid speaks side ballot othername schname regname regcode region pullup bracketseed won lost/);
 
 Tab::Entry->has_a(school => 'Tab::School');
 Tab::Entry->has_a(tourn => 'Tab::Tourn');
