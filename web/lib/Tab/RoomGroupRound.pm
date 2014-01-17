9e9bdc6cb7368af5901c65d0b7b8e4615208ffaf
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2014-01-17 04:52:40 +0000

    Full changes to room pools

--- web/lib/Tab/RoomGroupRound.pm
+++ web/lib/Tab/RoomGroupRound.pm
@@ -1,7 +1,7 @@
-package Tab::RoundGroupRound;
+package Tab::RoomGroupRound;
 use base 'Tab::DBI';
-Tab::RoundGroupRound->table('room_group_round');
-Tab::RoundGroupRound->columns(All => qw/id round round_group timestamp/);
-Tab::RoundGroupRound->has_a(round => 'Tab::Round');
+Tab::RoomGroupRound->table('room_group_round');
+Tab::RoomGroupRound->columns(All => qw/id round room_group timestamp/);
+Tab::RoomGroupRound->has_a(round => 'Tab::Round');
 
 __PACKAGE__->_register_datetimes( qw/timestamp/);
