281f0c18759cefdbeecd07a23cebf88fdebccb75
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2014-02-09 16:13:58 +0000

    better utility scripts

--- doc/utility/rsync-files.sh
+++ doc/utility/rsync-files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ln -s /bin/chmod /usr/bin/chmod > /dev/null 2>&1
+ln -s /bin/chmod /usr/bin/chmod
 rsync -avzru amazon@azuen.net:/home/palmer/Dropbox/Tech/Backup/files /www/
 rsync -avzru /www/files amazon@azuen.net:/home/palmer/Dropbox/Tech/Backup/
 
