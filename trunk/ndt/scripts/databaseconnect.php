3cad91a41e98dcaa5966b9cd2bceca7dfc354368
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2014-02-11 20:52:19 +0000

    Database

--- trunk/ndt/scripts/databaseconnect.php
+++ trunk/ndt/scripts/databaseconnect.php
@@ -6,7 +6,7 @@
 	$username="tabroom";
 	$password="Ev34yGYLMPGYP3Y4";
 	$database="itab";
-	$host="tabroom.coha3i31zrbw.us-east-1.rds.amazonaws.com"
+	$host="tabroom.coha3i31zrbw.us-east-1.rds.amazonaws.com";
 
 	mysql_connect($host,$username,$password);
 	@mysql_select_db($database) or die( "Unable to select database<br>");
