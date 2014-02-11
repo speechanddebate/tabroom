86d14675820719e40c6c1dc9c46bee369c2e7924
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2014-02-11 21:04:08 +0000

    Something

--- doc/utility/rsync-files.sh
+++ doc/utility/rsync-files.sh
@@ -2,4 +2,6 @@
 ln -s /bin/chmod /usr/bin/chmod
 rsync -avzru amazon@azuen.net:/home/palmer/Dropbox/Tech/Backup/files /www/
 rsync -avzru /www/files amazon@azuen.net:/home/palmer/Dropbox/Tech/Backup/
+chown -R www-data.www.data /www/files
+
 
