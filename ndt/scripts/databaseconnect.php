aa67b96ff8ce176811a1f5dc9e561734fa2abcc3
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2014-02-11 20:51:34 +0000

    Database

--- ndt/scripts/databaseconnect.php
+++ ndt/scripts/databaseconnect.php
@@ -1,8 +1,14 @@
 <?php
-$username="jbruschke";
-$password="Oof9iyeeGh9jeeg";
-$database="itab";
+#$username="jbruschke";
+#$password="Oof9iyeeGh9jeeg";
+#$database="itab";
+#$host="localhost";
+	$username="tabroom";
+	$password="Ev34yGYLMPGYP3Y4";
+	$database="itab";
+	$host="tabroom.coha3i31zrbw.us-east-1.rds.amazonaws.com"
 
-mysql_connect("localhost",$username,$password);
-@mysql_select_db($database) or die( "Unable to select database<br>");
+	mysql_connect($host,$username,$password);
+	@mysql_select_db($database) or die( "Unable to select database<br>");
 ?>
+
