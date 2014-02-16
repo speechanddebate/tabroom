b254a63b18d3120960a379ace49033f251a9db5c
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2014-02-16 04:19:26 +0000

    Stat

--- trunk/ndt/databaseconnect.php
+++ trunk/ndt/databaseconnect.php
@@ -1,8 +1,13 @@
 <?php
-$username="jbruschke";
-$password="Oof9iyeeGh9jeeg";
-$database="itab";
+#	$username="jbruschke";
+#	$password="Oof9iyeeGh9jeeg";
+#	$database="itab";
+#	$host="localhost";
 
-mysql_connect("localhost",$username,$password);
-@mysql_select_db($database) or die( "Unable to select database<br>");
+	$username="tabroom";
+	$password="Ev34yGYLMPGYP3Y4";
+	$database="itab";
+	$host="tabroom2.coha3i31zrbw.us-east-1.rds.amazonaws.com";
+	mysql_connect($host,$username,$password);
+	@mysql_select_db($database) or die( "Unable to select database<br>");
 ?>
