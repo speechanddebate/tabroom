bfdc514682679c48dc48365698d7d5dbe70afe36
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2014-02-19 15:52:26 +0000

    DB back

--- trunk/ndt/scripts/databaseconnect.php
+++ trunk/ndt/scripts/databaseconnect.php
@@ -1,13 +1,12 @@
 <?php
-#$username="jbruschke";
-#$password="Oof9iyeeGh9jeeg";
-#$database="itab";
-#$host="localhost";
-	$username="tabroom";
-	$password="Ev34yGYLMPGYP3Y4";
+	$username="jbruschke";
+	$password="Oof9iyeeGh9jeeg";
 	$database="itab";
-	$host="tabroom2.coha3i31zrbw.us-east-1.rds.amazonaws.com";
-
+	$host="localhost";
+#	$username="tabroom";
+#	$password="Ev34yGYLMPGYP3Y4";
+#	$database="itab";
+#	$host="tabroom2.coha3i31zrbw.us-east-1.rds.amazonaws.com";
 	mysql_connect($host,$username,$password);
 	@mysql_select_db($database) or die( "Unable to select database<br>");
 ?>
