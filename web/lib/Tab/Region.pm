8bcc1e2d3c31fa21737a725ce2eb34fa2774b02b
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2014-04-22 03:36:56 +0000

    Yes there are no tomatoes

--- web/lib/Tab/Region.pm
+++ web/lib/Tab/Region.pm
@@ -3,10 +3,11 @@ use base 'Tab::DBI';
 Tab::Region->table('region');
 Tab::Region->columns(Primary => qw/id/);
 Tab::Region->columns(Essential => qw/circuit name code timestamp/);
-Tab::Region->columns(Others => qw/diocese quota arch sweeps cooke_pts/);
+Tab::Region->columns(Others => qw/diocese quota arch sweeps cooke_pts tourn/);
 Tab::Region->columns(TEMP => qw/registered unregistered/);
 
 Tab::Region->has_a(circuit => 'Tab::Circuit');
+Tab::Region->has_a(tourn => 'Tab::Tourn');
 
 Tab::Region->has_many(schools => 'Tab::School', 'region');
 Tab::Region->has_many(fines => 'Tab::RegionFine', 'region');
