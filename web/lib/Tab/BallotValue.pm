9843276196f5e846ddad2ecaa59423226024d82d
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2014-10-10 15:37:02 +0000

    Yes

--- web/lib/Tab/BallotValue.pm
+++ web/lib/Tab/BallotValue.pm
@@ -3,7 +3,7 @@ use base 'Tab::DBI';
 Tab::BallotValue->table('ballot_value');
 Tab::BallotValue->columns(Primary => qw/id/);
 Tab::BallotValue->columns(Essential => qw/ballot tag student value/);
-Tab::BallotValue->columns(Others => qw/timestamp content cat_id/);
+Tab::BallotValue->columns(Others => qw/timestamp content cat_id tiebreak/);
 Tab::BallotValue->columns(TEMP => qw/panelid entryid roundtype roundid studentid judgeid bye ballotid chair pullup/);
 
 Tab::BallotValue->has_a(ballot => 'Tab::Ballot');
