25be28e4327fbe90f95d9dee5ca2b43126a7516e
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2014-11-18 01:23:34 +0000

    Yes we have no tomatoes

--- web/lib/Tab/RoomGroupRoom.pm
+++ web/lib/Tab/RoomGroupRoom.pm
@@ -3,5 +3,6 @@ use base 'Tab::DBI';
 Tab::RoomGroupRoom->table('room_group_room');
 Tab::RoomGroupRoom->columns(All => qw/id room room_group timestamp/);
 Tab::RoomGroupRoom->has_a(room => 'Tab::Room');
+Tab::RoomGroupRoom->has_a(room_group => 'Tab::RoomGroup');
 
 __PACKAGE__->_register_datetimes( qw/timestamp/);
