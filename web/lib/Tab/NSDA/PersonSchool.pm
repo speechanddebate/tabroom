ead4bd345a0c8e7da608a72e80dde058004bf5d0
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2015-06-13 14:21:39 +0000

    Contact email exposed

--- web/lib/Tab/NSDA/PersonSchool.pm
+++ web/lib/Tab/NSDA/PersonSchool.pm
@@ -1,6 +1,6 @@
 package Tab::NSDA::PersonSchool;
 use base 'Tab::NSDA::PointsDBI';
-Tab::NSDA::PersonSchool->table('NEW_USERS_TO_SCHOOL');
+Tab::NSDA::PersonSchool->table('NEW_USERS_TO_SCHOOLS');
 Tab::NSDA::PersonSchool->columns(Essential => qw/ualt_id school_id startdate enddate tostrength hasaccess hasaccessdate accessgranted isadmin/);
 
 Tab::NSDA::PersonSchool->has_a( school_id => "Tab::NSDA::MemberSchool");
