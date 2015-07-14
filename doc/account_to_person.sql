226cbe189545ce2c4fca1404bfe7a4eea7e5567e
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2015-07-14 04:03:00 +0000

    Many massive changes.  Still untested

---
+++ doc/account_to_person.sql
@@ -0,0 +1,14 @@
+
+alter table chapter_admin change account person int;
+alter table judge change account person int;
+alter table chapter_judge change account person int;
+alter table circuit_admin change account person int;
+alter table account_conflict change account person int;
+alter table student change account person int;
+alter table tourn_admin change account person int;
+alter table tourn_change change account person int;
+alter table ballot change account person int;
+
+
+
+
