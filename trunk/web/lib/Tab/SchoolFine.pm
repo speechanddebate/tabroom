81c85cb749d64a6986560a6dc2d7a08a51d49f07
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2015-08-25 23:36:32 +0000

    Many many fixes, new invoice styles, the works

--- trunk/web/lib/Tab/SchoolFine.pm
+++ trunk/web/lib/Tab/SchoolFine.pm
@@ -1,7 +1,7 @@
 package Tab::SchoolFine;
 use base 'Tab::DBI';
 Tab::SchoolFine->table('school_fine');
-Tab::SchoolFine->columns(All => qw/id school tourn amount reason levied_on levied_by timestamp region judge payment deleted deleted_by deleted_on/);
+Tab::SchoolFine->columns(All => qw/id school tourn amount reason levied_on levied_by timestamp region judge payment deleted deleted_by deleted_at/);
 
 Tab::SchoolFine->has_a(school => 'Tab::School');
 Tab::SchoolFine->has_a(region => 'Tab::Region');
@@ -10,4 +10,4 @@ Tab::SchoolFine->has_a(tourn => 'Tab::Tourn');
 Tab::SchoolFine->has_a(levied_by => 'Tab::Account');
 Tab::SchoolFine->has_a(deleted_by => 'Tab::Account');
 
-__PACKAGE__->_register_datetimes( qw/levied_on deleted_on timestamp/);
+__PACKAGE__->_register_datetimes( qw/levied_on deleted_at timestamp/);
