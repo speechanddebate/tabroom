7b04c9d47ee4da4ca492bfba25366650e1463282
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2015-09-09 21:34:37 +0000

    Lots of edits more

--- web/lib/Tab/Entry.pm
+++ web/lib/Tab/Entry.pm
@@ -10,9 +10,6 @@ Tab::Entry->has_a(school => 'Tab::School');
 Tab::Entry->has_a(tourn => 'Tab::Tourn');
 Tab::Entry->has_a(event => 'Tab::Event');
 
-Tab::Entry->has_a(reg_by => 'Tab::Account');
-Tab::Entry->has_a(drop_by => 'Tab::Account');
-
 Tab::Entry->has_many(strikes => 'Tab::Strike', 'entry');
 Tab::Entry->has_many(ballots => 'Tab::Ballot', 'entry');
 Tab::Entry->has_many(changes => 'Tab::TournChange', 'entry');
@@ -21,9 +18,7 @@ Tab::Entry->has_many(qualifiers => 'Tab::Qualifier', 'entry');
 Tab::Entry->has_many(entry_students => 'Tab::EntryStudent', 'entry');
 Tab::Entry->has_many(students => [Tab::EntryStudent => 'student']);
 
-__PACKAGE__->_register_datetimes( qw/timestamp/);
-__PACKAGE__->_register_datetimes( qw/drop_time/);
-__PACKAGE__->_register_datetimes( qw/reg_time/);
+__PACKAGE__->_register_datetimes( qw/created_at timestamp/);
 
 sub add_student { 
 	my ($self, $student) = @_;
