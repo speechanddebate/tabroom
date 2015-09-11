ccbac680fda4e8d27a2b53bc1733b1cec0894e9c
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2015-09-11 15:32:22 +0000

    Minor fixes

--- web/lib/Tab/Round.pm
+++ web/lib/Tab/Round.pm
@@ -3,16 +3,14 @@ use base 'Tab::DBI';
 Tab::Round->table('round');
 Tab::Round->columns(Primary => qw/id/);
 Tab::Round->columns(Essential => qw/name label event type timeslot site pool/);
-Tab::Round->columns(Others => qw/post_results published listed created completed 
-									blasted timestamp tb_set motion judges cat_id 
-									flighted start_time note wipe_rooms ignore_results
-									sidelock_against include_room_notes/);
+Tab::Round->columns(Others => qw/post_results published created timestamp tb_set
+								flighted start_time/);
 Tab::Round->columns(TEMP => qw/speaks/);
 
-Tab::Round->has_a(event => 'Tab::Event');
 Tab::Round->has_a(site => 'Tab::Site');
-Tab::Round->has_a(timeslot => 'Tab::Timeslot');
+Tab::Round->has_a(event => 'Tab::Event');
 Tab::Round->has_a(tb_set => 'Tab::TiebreakSet');
+Tab::Round->has_a(timeslot => 'Tab::Timeslot');
 
 Tab::Round->has_many(jpools => [Tab::JPoolRound => 'jpool']);
 Tab::Round->has_many(rpools => [Tab::RPoolRound => 'rpool']);
@@ -20,11 +18,7 @@ Tab::Round->has_many(rpools => [Tab::RPoolRound => 'rpool']);
 Tab::Round->has_many(panels => 'Tab::Panel', 'round' => { order_by => 'letter'} );
 Tab::Round->has_many(results => 'Tab::Result', 'round');
 
-__PACKAGE__->_register_datetimes( qw/blasted/);
-__PACKAGE__->_register_datetimes( qw/created/);
-__PACKAGE__->_register_datetimes( qw/completed/);
-__PACKAGE__->_register_datetimes( qw/timestamp/);
-__PACKAGE__->_register_datetimes( qw/start_time/);
+__PACKAGE__->_register_datetimes( qw/created start_time timestamp/);
 
 sub realname { 
 	my $self = shift;
