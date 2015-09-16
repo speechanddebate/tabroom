6630966da208684a8d60b6c288319805b9992b88
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2015-09-16 14:08:26 +0000

    All the things

--- web/lib/Tab/RPool.pm
+++ web/lib/Tab/RPool.pm
@@ -2,11 +2,12 @@ package Tab::RPool;
 use base 'Tab::DBI';
 Tab::RPool->table('rpool');
 Tab::RPool->columns(All => qw/id name tourn timestamp/);
-Tab::RPool->has_many(rounds => [ Tab::RPoolRound => 'round'], 'rpool');
-Tab::RPool->has_many(rooms => [ Tab::RPoolRoom => 'room'], 'rpool');
 Tab::RPool->has_many(rpools => 'Tab::RPoolRoom','rpool');
 Tab::RPool->has_many(round_links => 'Tab::RPoolRound','rpool');
 Tab::RPool->has_many(room_links => 'Tab::RPoolRoom','rpool');
+
+Tab::RPool->has_many(rounds => [ Tab::RPoolRound => 'round'], 'round');
+Tab::RPool->has_many(rooms => [ Tab::RPoolRoom => 'room'], 'room');
 
 __PACKAGE__->_register_datetimes( qw/timestamp/);
 
