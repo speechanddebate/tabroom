93fed25e05d88bf20f748f04df6ad0e1a4a32ec5
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2016-04-14 00:33:07 +0000

    better times

--- web/lib/Tab/Login.pm
+++ web/lib/Tab/Login.pm
@@ -2,7 +2,7 @@ package Tab::Login;
 use base 'Tab::DBI';
 Tab::Login->table('login');
 Tab::Login->columns(Primary => qw/id/);
-Tab::Login->columns(Essential => qw/username password salt name person/);
+Tab::Login->columns(Essential => qw/username password salt sha512 name person/);
 Tab::Login->columns(Others => qw/accesses last_access pass_changekey pass_timestamp pass_change_expires source timestamp ualt_id nsda_login_id/);
 
 Tab::Login->has_a(person => 'Tab::Account');
