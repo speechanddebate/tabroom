ee0af25b4a0732c4b9ca3524c15d83ef3029d377
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2016-05-10 13:46:35 +0000

    Backup scripts direct in svn

---
+++ doc/scripts/backup-tabroom-hourly.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+use DateTime;
+
+my $dt = DateTime->now;
+my $hour = $dt->hour;
+
+my $db = "tabroom";
+my $backup_dir = "/home/palmer/backups/sqlfiles";
+
+my $sql_file = "$backup_dir/$db.sql";
+
+`rm $backup_dir/*.bz2`;
+
+print "BACKING UP $db to $sql_file....";
+`/bin/mkdir -p $backup_dir`;
+`/usr/bin/mysqldump $db > $sql_file`;
+print "...done.\n";
+
+print "\nCompressing.....";
+`/bin/bzip2 $sql_file`;
+print "...done.\n";
+
+print "\nPermissions:....";
+`/bin/chmod -R 660 $sql_file.bz2`;
+print "...done. ";
+
+print "\nCopying $sql_file.bz2 to S3:....";
+`/usr/bin/s3cmd put $sql_file.bz2 s3://tabroom-db/hourly/$hour-$db.sql.bz2`;
+`/usr/bin/s3cmd put $sql_file.bz2 s3://tabroom-db/latest/$db.sql.bz2`;
+print "...done. ";
+
+print "\nBackup of $db completed on ".$dt->mdy('/')." at ".$dt->hms(':')." \n";
+
---
+++ doc/scripts/backup-tabroom.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+
+use DateTime;
+
+my @databases = "tabroom";
+
+my $dt = DateTime->now;
+my $date = $dt->ymd;
+chomp $date;
+
+print "Dumping databases to file:";
+
+my $garbage;
+my $backup_dir = "/home/palmer/backups/sqlfiles";
+
+`rm $backup_dir/*.bz2`;
+
+foreach my $db (@databases) { 
+
+	chomp $db;
+	my $sql_file = "$backup_dir/$db.sql";
+	print "BACKING UP $db to $sql_file....";
+	`/bin/rm $sql_file > /dev/null 2>&1`;
+	`/bin/rm $sql_file.bz2 > /dev/null 2>&1`;
+
+	`/bin/mkdir -p $backup_dir`;
+	`/usr/bin/mysqldump $db > $sql_file`;
+	print "...done.\n";
+
+	print "\nCompressing.....";
+	`/bin/bzip2 $sql_file`;
+	print "...done.\n";
+
+	print "\nPermissions:....";
+	`/bin/chmod -R 660 $sql_file.bz2`;
+	print "...done.\n";
+
+	print "\nCopying to S3:....";
+	`/usr/bin/s3cmd put $sql_file.bz2 s3://tabroom-db/$date/$db.sql.bz2`;
+	`/usr/bin/s3cmd put $sql_file.bz2 s3://tabroom-db/latest/$db.sql.bz2`;
+	print "...done.\n";
+
+}
+
+print "\nBackup of databases completed on ".$dt->mdy('/')." at ".$dt->hms(':')." \n";
+
+# Delete the backup from 2 weeks ago, unless it's the one from the 1st day of the month.
+my $delete_day = $dt->clone;
+$delete_day->subtract(days => 14);
+
+unless ($delete_day->day == 1) {
+	print "Deleting the backup from two weeks ago:";
+	my $delete_date = $delete_day->ymd;
+	`/usr/bin/s3cmd del -r s3://tabroom-db/$delete_date`;
+	print "..done\n";
+}
+
