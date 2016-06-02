fb5242f1470efe27ecb43fd43a0d1a1b3953c338
palmer <palmer@fa0cfba1-3b41-4c9c-a089-e5021995bac7>
2016-06-02 20:21:49 +0000

    Added a little JS control to the side bar.  Increased font sizes.  Setting standardization infrastructure

--- web/lib/Tab/JPoolSetting.pm
+++ web/lib/Tab/JPoolSetting.pm
@@ -1,7 +1,7 @@
 package Tab::JPoolSetting;
 use base 'Tab::DBI';
 Tab::JPoolSetting->table('jpool_setting');
-Tab::JPoolSetting->columns(All => qw/id jpool tag value value_date value_text timestamp/);
+Tab::JPoolSetting->columns(All => qw/id jpool tag value value_date value_text setting timestamp/);
 Tab::JPoolSetting->has_a(jpool => 'Tab::JPool');
 
 __PACKAGE__->_register_datetimes( qw/timestamp/);
--- web/lib/Tab/RegionSetting.pm
+++ web/lib/Tab/RegionSetting.pm
@@ -1,7 +1,7 @@
 package Tab::RegionSetting;
 use base 'Tab::DBI';
 Tab::RegionSetting->table('region_setting');
-Tab::RegionSetting->columns(All => qw/id region tag value value_date value_text event timestamp/);
+Tab::RegionSetting->columns(All => qw/id region tag value value_date value_text event setting timestamp/);
 Tab::RegionSetting->has_a(region => 'Tab::Region');
 Tab::RegionSetting->has_a(event => 'Tab::Event');
 
