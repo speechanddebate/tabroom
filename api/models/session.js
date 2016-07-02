0f977a27a04724a69d4e0012e6c1d0811a288903
Chris Palmer <palmer@tabroom.com>
2016-07-02 16:46:40 -0400

    Routes and data definitions run.  Logins cross.

---
+++ api/models/session.js
@@ -0,0 +1,17 @@
+/* jshint indent: 4 */
+
+module.exports = function(sequelize, DataTypes) {
+	return sequelize.define('session', { 
+		userkey: { 
+			type: DataTypes.STRING(127),
+			allowNull: false
+		},
+		ip: { 
+			type: DataTypes.STRING(63),
+			allowNull: false
+		}
+	});
+};
+
+
+
