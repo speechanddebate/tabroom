4e5bd4c82594d21c7ba65db94c56102f74b2cdbf
Hera Production <palmer@tabroom.com>
2016-07-04 14:40:29 -0400

    Little last minute bug fixes

--- web/lib/Tab/ResultValue.pm
+++ web/lib/Tab/ResultValue.pm
@@ -3,7 +3,7 @@ use base 'Tab::DBI';
 Tab::ResultValue->table('result_value');
 Tab::ResultValue->columns(Primary => qw/id/);
 Tab::ResultValue->columns(Essential => qw/result tag value priority sort_desc no_sort/);
-Tab::ResultValue->columns(Others => qw/long_tag timestamp /);
+Tab::ResultValue->columns(Others => qw/description timestamp /);
 Tab::ResultValue->has_a(result => 'Tab::Result');
 
 __PACKAGE__->_register_datetimes( qw/timestamp/);
