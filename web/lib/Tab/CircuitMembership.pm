7bc3aeebfe062af0044887e077d451974437ae3c
Hera Production <palmer@tabroom.com>
2016-07-07 14:29:10 -0400

    fly fixes

--- web/lib/Tab/CircuitMembership.pm
+++ web/lib/Tab/CircuitMembership.pm
@@ -2,7 +2,7 @@ package Tab::CircuitMembership;
 use base 'Tab::DBI';
 Tab::CircuitMembership->table('circuit_membership');
 Tab::CircuitMembership->columns(Primary => qw/id/);
-Tab::CircuitMembership->columns(Essential => qw/name circuit description approval_required region_required timestamp/);
+Tab::CircuitMembership->columns(Essential => qw/name circuit description approval timestamp/);
 Tab::CircuitMembership->has_a(circuit => "Tab::Circuit");
 Tab::CircuitMembership->has_many(chapter_circuit_memberships => "Tab::ChapterCircuit", "circuit_membership");
 __PACKAGE__->_register_datetimes( qw/timestamp/);
