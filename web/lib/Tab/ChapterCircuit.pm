fb474adc045d8608d63f8de251909281eadd3be2
Hera Production <palmer@tabroom.com>
2016-07-09 13:23:33 -0400

    Minor schema problems

--- web/lib/Tab/ChapterCircuit.pm
+++ web/lib/Tab/ChapterCircuit.pm
@@ -2,7 +2,7 @@ package Tab::ChapterCircuit;
 use base 'Tab::DBI';
 Tab::ChapterCircuit->table('chapter_circuit');
 Tab::ChapterCircuit->columns(Primary => qw/id/);
-Tab::ChapterCircuit->columns(Essential => qw/circuit chapter code circuit_membership region/);
+Tab::ChapterCircuit->columns(Essential => qw/circuit chapter code full_member circuit_membership region/);
 Tab::ChapterCircuit->has_a(circuit => "Tab::Circuit");
 Tab::ChapterCircuit->has_a(chapter => "Tab::Chapter");
 Tab::ChapterCircuit->has_a(region => "Tab::Region");
