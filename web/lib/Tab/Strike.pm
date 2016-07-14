d5c9de077f2a0e84798cc82a8fd8306e1329fc67
Chris Palmer <palmer@tabroom.com>
2016-07-13 22:08:48 -0400

    Fixed some horkage in the init scripts

--- web/lib/Tab/Strike.pm
+++ web/lib/Tab/Strike.pm
@@ -6,19 +6,18 @@ Tab::Strike->columns(Essential => qw/type start end registrant conflictee
 							tourn judge event entry school region timeslot strike_timeslot 
 							entered_by timestamp/);
 
-Tab::Strike->has_a(judge => 'Tab::Judge');
 Tab::Strike->has_a(tourn => 'Tab::Tourn');
+Tab::Strike->has_a(judge => 'Tab::Judge');
 Tab::Strike->has_a(event => 'Tab::Event');
 Tab::Strike->has_a(entry => 'Tab::Entry');
+
 Tab::Strike->has_a(school => 'Tab::School');
 Tab::Strike->has_a(region => 'Tab::Region');
 Tab::Strike->has_a(timeslot => 'Tab::Timeslot');
 Tab::Strike->has_a(strike_timeslot => 'Tab::StrikeTimeslot');
-
 Tab::Strike->has_a(entered_by => 'Tab::Person');
 
-__PACKAGE__->_register_datetimes( qw/start/ );
-__PACKAGE__->_register_datetimes( qw/end/);
+__PACKAGE__->_register_datetimes( qw/start end timestamp/);
 
 sub name { 
 	my $self = shift;
