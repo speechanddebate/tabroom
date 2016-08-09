e2c14ab1a7d7b0c24272d957f6f9a1e8cee84756
Chris Palmer <palmer@tabroom.com>
2016-08-09 17:25:55 -0400

    New logins stuff

--- web/lib/Tab/Chapter.pm
+++ web/lib/Tab/Chapter.pm
@@ -2,7 +2,7 @@ package Tab::Chapter;
 use base 'Tab::DBI';
 Tab::Chapter->table('chapter');
 Tab::Chapter->columns(Primary => qw/id/);
-Tab::Chapter->columns(Essential => qw/name address city state zip postal country/);
+Tab::Chapter->columns(Essential => qw/name street city state zip postal country/);
 Tab::Chapter->columns(Others => qw/coaches self_prefs level naudl ipeds nces ceeb nsda timestamp/);
 Tab::Chapter->columns(TEMP => qw/count prefs code member schoolid/);
 
