02db60d302739a655bef0a910ac32a53a4fbc55f
Chris Palmer <palmer@tabroom.com>
2016-10-19 13:53:51 -0400

    districts importation and other fun

--- web/lib/Tab/District.pm
+++ web/lib/Tab/District.pm
@@ -3,6 +3,7 @@ use base 'Tab::DBI';
 Tab::District->table('district');
 Tab::District->columns(Primary => qw/id/);
 Tab::District->columns(Essential => qw/name code location chair timestamp/);
+Tab::District->columns(Others => qw/level/);
 
 Tab::District->has_many(schools => 'Tab::School', 'district');
 Tab::District->has_many(tourns => 'Tab::Tourn', 'district');
