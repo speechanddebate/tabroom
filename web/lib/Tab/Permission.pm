86dcb60ff82799223aa77e6b6d240275dade83ba
Chris Palmer <palmer@tabroom.com>
2016-11-16 11:26:53 -0500

    Perms

--- web/lib/Tab/Permission.pm
+++ web/lib/Tab/Permission.pm
@@ -1,9 +1,10 @@
 package Tab::Permission;
 use base 'Tab::DBI';
 Tab::Permission->table('permission');
-Tab::Permission->columns(All   => qw/id tag person category tourn chapter region circuit timestamp/);
+Tab::Permission->columns(All   => qw/id tag person category tourn chapter region district circuit timestamp/);
 
 Tab::Permission->has_a(circuit => "Tab::Circuit");
+Tab::Permission->has_a(district => "Tab::District");
 Tab::Permission->has_a(tourn   => "Tab::Tourn");
 Tab::Permission->has_a(region  => "Tab::Region");
 Tab::Permission->has_a(category   => "Tab::Category");
