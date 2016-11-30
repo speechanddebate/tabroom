715350bac28b0302e46c8095d1cba443ecdc065e
Hera Production <palmer@tabroom.com>
2016-11-29 23:08:23 -0500

    Messages

--- web/lib/Tab/Entry.pm
+++ web/lib/Tab/Entry.pm
@@ -16,6 +16,7 @@ Tab::Entry->has_a(event => 'Tab::Event');
 Tab::Entry->has_a(registered_by => 'Tab::Person');
 
 Tab::Entry->has_many(strikes => 'Tab::Strike', 'entry');
+Tab::Entry->has_many(settings => 'Tab::EntrySetting', 'entry');
 Tab::Entry->has_many(ballots => 'Tab::Ballot', 'entry');
 Tab::Entry->has_many(changes => 'Tab::ChangeLog', 'entry');
 Tab::Entry->has_many(ratings => 'Tab::Rating', 'entry');
@@ -26,9 +27,17 @@ Tab::Entry->has_many(students => [Tab::EntryStudent => 'student']);
 __PACKAGE__->_register_datetimes( qw/timestamp created_at/);
 
 sub add_student { 
+
 	my ($self, $student) = @_;
-	my @existing = Tab::EntryStudent->search( student => $student, entry => $self->id );
-	Tab::EntryStudent->create({ student => $student, entry => $self->id }) unless @existing;
+
+	my @existing = Tab::EntryStudent->search( 
+		student => $student,
+		entry   => $self->id
+	);
+
+	Tab::EntryStudent->create({ 
+		student => $student, 
+		entry => $self->id }) unless @existing;
 	return;
 }
 
@@ -49,7 +58,7 @@ sub setting {
 
 	my $existing = Tab::EntrySetting->search(entry => $self->id, tag => $tag)->first;
 
-	if (defined $value) { 
+	if (defined $value) {
 			
 		if ($existing) {
 
@@ -92,6 +101,24 @@ sub setting {
 		return $existing->value;
 
 	}
+
+}
+
+sub all_settings { 
+
+	my $self = shift;
+
+	my @settings = $self->settings;
+
+	my %all_settings;
+
+	foreach my $setting (@settings) { 
+		$all_settings{$setting->tag} = $setting->value;
+		$all_settings{$setting->tag} = $setting->value_text if $all_settings{$setting->tag} eq "text";
+		$all_settings{$setting->tag} = $setting->value_date if $all_settings{$setting->tag} eq "date";
+	}
+
+	return %all_settings;
 
 }
 
