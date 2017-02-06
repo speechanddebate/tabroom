00bb9b88706a12422d0281e08a37c22fbf6fc022
Chris Palmer <palmer@tabroom.com>
2017-02-06 15:23:52 -0500

    Cleaned up the partial judges; elim ballots at Districts, som eother stuff

--- web/lib/Tab/StrikeTimeslot.pm
+++ web/lib/Tab/StrikeTimeslot.pm
@@ -2,7 +2,7 @@ package Tab::StrikeTimeslot;
 use base 'Tab::DBI';
 Tab::StrikeTimeslot->table('strike_timeslot');
 Tab::StrikeTimeslot->columns(Primary => qw/id/);
-Tab::StrikeTimeslot->columns(Essential => qw/name fine start end category timestamp/);
+Tab::StrikeTimeslot->columns(Essential => qw/name fine start end category no_hires timestamp/);
 
 Tab::StrikeTimeslot->has_a(category => 'Tab::Category');
 Tab::StrikeTimeslot->has_many(strikes => 'Tab::Strike', "strike_timeslot");
