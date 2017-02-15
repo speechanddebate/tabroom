d360be1345e375039da6a807f349815a010bbffa
Chris Palmer <palmer@tabroom.com>
2017-02-15 16:30:58 -0500

    fixed a ton of things and added sizes and types to concessions

--- web/lib/Tab/ConcessionPurchase.pm
+++ web/lib/Tab/ConcessionPurchase.pm
@@ -1,7 +1,22 @@
 package Tab::ConcessionPurchase;
 use base 'Tab::DBI';
 Tab::ConcessionPurchase->table('concession_purchase');
-Tab::ConcessionPurchase->columns(Essential => qw/id concession quantity school placed fulfilled timestamp/);
+Tab::ConcessionPurchase->columns(Essential => qw/id 
+		concession 
+		quantity 
+		school 
+		placed fulfilled 
+		timestamp
+	/);
 Tab::ConcessionPurchase->has_a(school => 'Tab::School');
 Tab::ConcessionPurchase->has_a(concession => 'Tab::Concession');
+
+Tab::ConcessionPurchase->has_many(purchase_options => 'Tab::ConcessionPurchaseOption', 'concession_purchase');
+
+Tab::ConcessionPurchase->has_many(
+	options => 
+		[Tab::ConcessionPurchaseOption => 'concession_option'], 
+		'concession_purchase'
+	);
+
 __PACKAGE__->_register_datetimes( qw/timestamp placed/);
