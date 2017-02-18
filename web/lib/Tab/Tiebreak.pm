6ab16663e206b1abf337012bbfea18c0cb9f5684
Chris Palmer <palmer@tabroom.com>
2017-02-17 19:27:20 -0500

    Doing the TB delete thing the right way now

--- web/lib/Tab/Tiebreak.pm
+++ web/lib/Tab/Tiebreak.pm
@@ -8,6 +8,7 @@ Tab::Tiebreak->columns(All => qw/id name child priority truncate
 								/);
 Tab::Tiebreak->has_a(tiebreak_set => 'Tab::TiebreakSet');
 Tab::Tiebreak->has_a(child => 'Tab::TiebreakSet');
+Tab::Tiebreak->has_many(rounds => 'Tab::Round', 'tiebreak_set' => { order_by => 'name'}  );
 
 __PACKAGE__->_register_datetimes( qw/timestamp/);
 
