be097dc33c839ad2914f1087446c12f3dba3b688
Chris Palmer <palmer@tabroom.com>
2017-03-29 18:13:15 -0400

    NSDA Points autopoint bug; still no congress.  NSDA Nationals entries posting

--- web/lib/Tab/SweepRule.pm
+++ web/lib/Tab/SweepRule.pm
@@ -1,7 +1,13 @@
 package Tab::SweepRule;
 use base 'Tab::DBI';
 Tab::SweepRule->table('sweep_rule');
-Tab::SweepRule->columns(All => qw/id sweep_set tag value place count timestamp/);
+
+Tab::SweepRule->columns(All => qw/id sweep_set tag 
+				value place count 
+				tiebreak_set timestamp
+	/);
+
 Tab::SweepRule->has_a(sweep_set => 'Tab::SweepSet');
+Tab::SweepRule->has_a(tiebreak_set => 'Tab::TiebreakSet');
 __PACKAGE__->_register_datetimes( qw/timestamp/);
 
