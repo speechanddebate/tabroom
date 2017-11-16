c0a553d04ccc7d9b08aef7c9138a44a7f3d31355
Chris Palmer <palmer@tabroom.com>
2017-11-15 23:41:45 -0500

    Better faster more productive.

--- web/lib/Tab/NSDA/MemberSchool.pm
+++ web/lib/Tab/NSDA/MemberSchool.pm
@@ -1,12 +1,15 @@
 package Tab::NSDA::MemberSchool;
 use base 'Tab::NSDA::PointsDBI';
 Tab::NSDA::MemberSchool->table('NEW_SCHOOLS');
-Tab::NSDA::MemberSchool->columns(Essential => qw/school_id school_name school_addr school_addr2
-										school_city school_state school_zip realm 
-										school_total_deg 
-										school_paid_status 
-										school_charter_status
-										school_trophy_points
-								/);
+Tab::NSDA::MemberSchool->columns(Essential => qw/school_id school_name 
+			realm 
+			school_addr school_addr2
+			school_city school_state school_zip 
+			school_phone
+			school_total_deg 
+			school_paid_status 
+			school_charter_status
+			school_trophy_points
+/);
 
 
