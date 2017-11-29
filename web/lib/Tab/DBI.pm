e70150a474df802a0825a3b603670658ad82af5e
Chris Palmer <palmer@tabroom.com>
2017-11-29 13:57:58 -0500

    Much improved congress schems; single-judge start times

--- web/lib/Tab/DBI.pm
+++ web/lib/Tab/DBI.pm
@@ -18,12 +18,14 @@ sub _register_datetimes {
 }
 
 sub date_inflate {
-   my $value = shift;
-   my $dt = eval { return DateTime::Format::MySQL->parse_datetime($value); };
-   $dt = DateTime->now unless $dt;
 
-   $dt->set_time_zone('UTC');
-   return $dt;
+	my $value = shift;
+	my $dt = eval { return DateTime::Format::MySQL->parse_datetime($value); };
+	$dt = DateTime->now unless $dt;
+
+	$dt->set_time_zone('UTC');
+	return $dt;
+
 }
 
 sub dateparse {
