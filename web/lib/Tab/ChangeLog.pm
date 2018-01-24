b8278c94764d05f597585d67caea9f706621cc78
Chris Palmer <palmer@tabroom.com>
2018-01-24 13:54:33 -0500

    Change log deletions

--- web/lib/Tab/ChangeLog.pm
+++ web/lib/Tab/ChangeLog.pm
@@ -3,7 +3,7 @@ use base 'Tab::DBI';
 Tab::ChangeLog->table('change_log');
 Tab::ChangeLog->columns(Primary => qw/id/);
 Tab::ChangeLog->columns(Essential => qw/type tourn school person description/);
-Tab::ChangeLog->columns(Others => qw/judge entry event new_panel old_panel fine  timestamp/);
+Tab::ChangeLog->columns(Others => qw/judge entry event new_panel old_panel fine deleted created timestamp/);
 
 Tab::ChangeLog->has_a(person => "Tab::Person");
 Tab::ChangeLog->has_a(tourn => "Tab::Tourn");
@@ -16,7 +16,7 @@ Tab::ChangeLog->has_a(fine => "Tab::Fine");
 Tab::ChangeLog->has_a(new_panel => "Tab::Panel");
 Tab::ChangeLog->has_a(old_panel => "Tab::Panel");
 
-__PACKAGE__->_register_datetimes( qw/timestamp/);
+__PACKAGE__->_register_datetimes( qw/timestamp created/);
 
 sub account { 
 	my $self = shift;
