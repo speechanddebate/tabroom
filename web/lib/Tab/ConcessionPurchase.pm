92c8bbaef6665eef51bf5f61350195e0fb436b72
Chris Palmer <palmer@tabroom.com>
2018-02-15 10:27:36 -0500

    Billing

--- web/lib/Tab/ConcessionPurchase.pm
+++ web/lib/Tab/ConcessionPurchase.pm
@@ -9,14 +9,21 @@ Tab::ConcessionPurchase->columns(Essential => qw/id
 		school 
 		placed 
 		fulfilled 
+		invoice
 		timestamp
 	/);
 
-Tab::ConcessionPurchase->has_a(school => 'Tab::School');
+Tab::ConcessionPurchase->has_a(
+	school => 'Tab::School'
+);
 
-Tab::ConcessionPurchase->has_a(concession => 'Tab::Concession');
+Tab::ConcessionPurchase->has_a(
+	concession => 'Tab::Concession'
+);
 
-Tab::ConcessionPurchase->has_many(purchase_options => 'Tab::ConcessionPurchaseOption', 'concession_purchase');
+Tab::ConcessionPurchase->has_many(
+	purchase_options => 'Tab::ConcessionPurchaseOption', 'concession_purchase'
+);
 
 Tab::ConcessionPurchase->has_many(
 	options => 
