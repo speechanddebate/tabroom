237147127f59c423dcd620aa9728a7da0a3c5214
Chris Palmer <palmer@tabroom.com>
2018-02-23 14:46:32 -0500

    There is any number of new bullshit items herein contained

--- web/lib/Tab/Concession.pm
+++ web/lib/Tab/Concession.pm
@@ -2,7 +2,7 @@ package Tab::Concession;
 use base 'Tab::DBI';
 Tab::Concession->table('concession');
 Tab::Concession->columns(Primary => qw/id/);
-Tab::Concession->columns(Essential => qw/name price tourn deadline description timestamp cap school_cap/);
+Tab::Concession->columns(Essential => qw/name price tourn deadline description timestamp cap school_cap billing_code/);
 
 Tab::Concession->has_a(tourn => 'Tab::Tourn');
 
