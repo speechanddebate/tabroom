d746f3b485666f30cbc985f54498ccfb52ee05c2
Chris Palmer <palmer@tabroom.com>
2018-03-07 01:06:05 -0500

    Happiness is.

--- web/lib/Tab/File.pm
+++ web/lib/Tab/File.pm
@@ -2,13 +2,16 @@ package Tab::File;
 use base 'Tab::DBI';
 Tab::File->table('file');
 Tab::File->columns(All => qw/id label filename type published coach uploaded 
-								tourn school event circuit webpage parent timestamp/);
-Tab::File->has_a(school => 'Tab::School');
-Tab::File->has_a(event => 'Tab::Event');
-Tab::File->has_a(tourn => 'Tab::Tourn');
-Tab::File->has_a(parent => 'Tab::File');
-Tab::File->has_a(circuit => 'Tab::Circuit');
-Tab::File->has_a(webpage => 'Tab::Webpage');
+								tourn school event district circuit webpage parent person timestamp/);
+
+Tab::File->has_a(school   => 'Tab::School');
+Tab::File->has_a(event    => 'Tab::Event');
+Tab::File->has_a(tourn    => 'Tab::Tourn');
+Tab::File->has_a(parent   => 'Tab::File');
+Tab::File->has_a(person   => 'Tab::Person');
+Tab::File->has_a(circuit  => 'Tab::Circuit');
+Tab::File->has_a(district => 'Tab::District');
+Tab::File->has_a(webpage  => 'Tab::Webpage');
 
 Tab::File->has_many(children => 'Tab::File', 'parent');
 
