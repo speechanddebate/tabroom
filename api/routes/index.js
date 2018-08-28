8999135597045c6804961866046b9e72c9fdaa5b
Chris Palmer <palmer@tabroom.com>
2018-08-28 17:23:05 -0400

    better ad display thing

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 var env    = process.env.NODE_ENV || "development";
 var config = require(__dirname + '/../config/config.json')[env];
-var db = require(__dirname + '/../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
