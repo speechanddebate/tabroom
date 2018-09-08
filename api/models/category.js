618ec8c184ec3700125ec485bd0f66891ca29b99
Chris Palmer <palmer@tabroom.com>
2018-09-07 21:04:20 -0400

    Dammit Byram

--- api/models/category.js
+++ api/models/category.js
@@ -10,7 +10,15 @@ module.exports = function(sequelize, DataTypes) {
 		abbr: {
 			type: DataTypes.STRING(15),
 			allowNull: true
-		}
+		},
+		tourn: {
+			type: DataTypes.INTEGER,
+			allowNull: false
+		},
+		pattern: {
+			type: DataTypes.INTEGER,
+			allowNull: false
+		},
 	});
 };
 
