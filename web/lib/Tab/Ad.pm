e6d1ae259eb71dd287d7425bb03ea4adf3760e5b
Chris Palmer <palmer@tabroom.com>
2018-12-03 17:58:35 -0500

    Ordering the ads

--- web/lib/Tab/Ad.pm
+++ web/lib/Tab/Ad.pm
@@ -2,7 +2,7 @@ package Tab::Ad;
 use base 'Tab::DBI';
 Tab::Ad->table('ad');
 Tab::Ad->columns(Primary => qw/id/);
-Tab::Ad->columns(Essential => qw/tag filename url start end approved approved_by person created_at timestamp/);
+Tab::Ad->columns(Essential => qw/tag filename url sort_order start end approved approved_by person created_at timestamp/);
 Tab::Ad->has_a(person => 'Tab::Person');
 Tab::Ad->has_a(approved_by => 'Tab::Person');
 
