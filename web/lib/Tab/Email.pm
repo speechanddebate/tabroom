a62d854e0782b4d40c19b2e2756f96c32b19a516
Chris Palmer <palmer@tabroom.com>
2019-02-03 19:03:33 -0500

    Rich requests for Stanford billing email

--- web/lib/Tab/Email.pm
+++ web/lib/Tab/Email.pm
@@ -2,7 +2,7 @@ package Tab::Email;
 use base 'Tab::DBI';
 Tab::Email->table('email');
 Tab::Email->columns(Primary => qw/id/);
-Tab::Email->columns(Essential => qw/sender content subject sent_at circuit sent_to tourn timestamp/);
+Tab::Email->columns(Essential => qw/sender content metadata subject sent_at circuit sent_to tourn timestamp/);
 Tab::Email->has_a(sender => 'Tab::Person');
 Tab::Email->has_a(tourn => 'Tab::Tourn');
 
