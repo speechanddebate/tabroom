4b4f34e6bd927046e5416895e47cb3b98e3e97aa
Chris Palmer <palmer@tabroom.com>
2019-02-05 18:06:52 -0500

    Ready for a test run

---
+++ doc/convert/ballot-side-fixes.sql
@@ -0,0 +1,45 @@
+
+	update score, ballot            
+		set score.speech = ballot.speechnumber          
+		where score.speech is null              
+		and ballot.speechnumber is not null             
+		and ballot.id = score.ballot;
+
+    update IGNORE
+        score, ballot, ballot b2
+        set score.ballot = ballot.id
+        where score.ballot = b2.id
+
+        and b2.entry = ballot.entry
+        and b2.judge = ballot.judge
+        and b2.panel = ballot.panel
+        and b2.id > ballot.id;
+
+    delete score
+        from score, ballot, score s2, ballot b2
+        where score.ballot = ballot.id
+            and s2.ballot = b2.id
+
+            and b2.id < ballot.id
+            and b2.judge = ballot.judge
+            and b2.entry = ballot.entry
+            and score.tag = s2.tag
+            and score.student = s2.student
+            and score.id != s2.id
+
+
+	delete from panel where round = 0;
+	delete from panel where round is null;
+	
+	delete from panel where not exists ( select round.id from round where round.id = panel.round);
+	delete from ballot where not exists ( select panel.id from panel where panel.id = ballot.panel);
+	delete from score where not exists ( select ballot.id from ballot where ballot.id = score.ballot);
+
+    delete ballot.*
+    from ballot, ballot b2
+    where ballot.entry = b2.entry
+		and ballot.judge = b2.judge
+		and not exists ( 
+			select score.id from score where score.ballot = ballot.id
+		);
+
