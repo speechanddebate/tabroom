7326fb10acd11ec78dc94fa60bf67df411c181e4
Chris Palmer <palmer@tabroom.com>
2019-02-09 19:46:31 -0800

    Full payment register

--- web/lib/Tab/Invoice.pm
+++ web/lib/Tab/Invoice.pm
@@ -3,6 +3,7 @@ use base 'Tab::DBI';
 Tab::Invoice->table('invoice');
 Tab::Invoice->columns(All => qw/id blusynergy blu_number paid total details school timestamp/);
 
-Tab::Invoice->has_a(school => 'Tab::School');
+Tab::Invoice->has_a(school      => 'Tab::School');
 Tab::Invoice->has_many(payments => 'Tab::Fine', "invoice");
 __PACKAGE__->_register_datetimes( qw/timestamp/);
+
