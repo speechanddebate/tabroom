c87c2b8342a044cbf40d07dcdd3fa3537db71a52
Chris Palmer <palmer@tabroom.com>
2019-02-09 17:05:24 -0800

    BluSynergy number IDs

--- web/lib/Tab/Invoice.pm
+++ web/lib/Tab/Invoice.pm
@@ -1,7 +1,7 @@
 package Tab::Invoice;
 use base 'Tab::DBI';
 Tab::Invoice->table('invoice');
-Tab::Invoice->columns(All => qw/id blusynergy paid total details school timestamp/);
+Tab::Invoice->columns(All => qw/id blusynergy blu_number paid total details school timestamp/);
 
 Tab::Invoice->has_a(school => 'Tab::School');
 Tab::Invoice->has_many(payments => 'Tab::Fine', "invoice");
