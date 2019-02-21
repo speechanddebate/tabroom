5139c1ddbfc7f307ce02aca0e0677ea0d1aa349d
Chris Palmer <palmer@tabroom.com>
2019-02-21 10:13:19 -0500

    PKD adjust judges factor

---
+++ doc/convert/judge_adjust.sql
@@ -0,0 +1,10 @@
+
+update event_setting set value = 90  where value = '-.1'  and tag = "adjust_judges_owed";
+update event_setting set value = 150  where value = '.5'  and tag = "adjust_judges_owed";
+update event_setting set value = 120  where value = '.2'  and tag = "adjust_judges_owed";
+update event_setting set value = 300  where value = '2'  and tag = "adjust_judges_owed";
+update event_setting set value = 200  where value = '1'  and tag = "adjust_judges_owed";
+update event_setting set value = 500  where value = '4'  and tag = "adjust_judges_owed";
+update event_setting set value = 112.5  where value = '.13' and tag = "adjust_judges_owed";
+update event_setting set value = 115  where value = '.15' and tag = "adjust_judges_owed";
+
