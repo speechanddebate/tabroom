562768122dc57bfdc1c5679d1de0931d8e43af5d
Chris Palmer <palmer@tabroom.com>
2019-07-26 15:07:32 -0400

    Bracket double eliminations.  online async ballot save.

---
+++ doc/convert/cleanup.sql
@@ -0,0 +1,37 @@
+
+alter table score drop tiebreak;
+alter table score drop cat_id;
+
+alter table ballot drop constraint uk_ballots;
+
+alter table ballot drop speechnumber;
+alter table ballot drop cat_id;
+
+alter table panel drop score;
+alter table panel drop cat_id;
+alter table panel drop confirmed;
+alter table panel drop label;
+
+drop table diocese;
+
+alter table region drop quota;
+alter table region drop arch;
+alter table region drop cooke_pts;
+alter table region drop sweeps;
+
+alter table login drop nsda_login_id;
+alter table login drop source;
+alter table login drop spinhash;
+
+alter table login drop ualt_id;
+alter table person drop ualt_id;
+alter table student drop ualt_id;
+alter table chapter_judge drop ualt_id;
+
+delete from judge_setting where tag = "ualt_id";
+delete from student_setting where tag = "ualt_id";
+
+alter table ballot drop collected;
+alter table ballot drop collected_by;
+alter table ballot drop seed;
+alter table ballot drop pullup;
