af4bde9bef95c3c4670391687eff86d77dc620ed
Chris Palmer <palmer@tabroom.com>
2019-08-18 15:25:16 -0400

    Standardized topics

---
+++ web/lib/Tab/Topic.pm
@@ -0,0 +1,8 @@
+package Tab::Topic;
+use base 'Tab::DBI';
+Tab::Topic->table('topic');
+Tab::Topic->columns(Primary => qw/id/);
+Tab::Topic->columns(Essential => qw/tag source event_type topic_text school_year sort_order created_by created_at timestamp/);
+
+__PACKAGE__->_register_datetimes( qw/created_at timestamp/);
+
