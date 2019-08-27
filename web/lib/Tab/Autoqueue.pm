c876ec241e425cbabed0fec15d404348fe6e0727
Chris Palmer <palmer@tabroom.com>
2019-08-27 18:23:54 -0400

    oops

---
+++ web/lib/Tab/Autoqueue.pm
@@ -0,0 +1,10 @@
+package Tab::Autoqueue;
+use base 'Tab::DBI';
+Tab::Autoqueue->table('autoqueue');
+Tab::Autoqueue->columns(Primary => qw/id/);
+Tab::Autoqueue->columns(Essential => qw/tag event round timestamp created_by created_at/);
+Tab::Autoqueue->has_a(round => 'Tab::Round');
+Tab::Autoqueue->has_a(event => 'Tab::Event');
+Tab::Autoqueue->has_a(created_by => 'Tab::Person');
+
+__PACKAGE__->_register_datetimes( qw/timestamp created_at/);
