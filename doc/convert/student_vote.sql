f2514f4d25c62f71ca99b2fa38ddc9f4c64d14f0
Chris Palmer <palmer@tabroom.com>
2019-11-12 18:12:43 -0500

    Districts pilot debate nonsense for 2019-2020

--- doc/convert/student_vote.sql
+++ doc/convert/student_vote.sql
@@ -7,3 +7,11 @@ update tiebreak set name = "student_rank" where name = "student_ranks";
 update tiebreak set name = "student_nominee" where name = "student_ballot";
 
 
+    delete tiebreak.*, tiebreak_set.*
+
+        from tiebreak, tiebreak_set, tourn
+        where tourn.start > "2019-09-01 00:00:00"
+        and tourn.id = tiebreak_set.tourn
+        and tiebreak_set.name = "Debate Final NSDA Points"
+        and tiebreak_set.id = tiebreak.tiebreak_set
+
