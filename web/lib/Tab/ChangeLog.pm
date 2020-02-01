9ddbc208a87d974390a3ce0226c5dc8eb26f6433
Chris Palmer <palmer@tabroom.com>
2020-01-31 20:29:43 -0600

    lots of fun

--- web/lib/Tab/ChangeLog.pm
+++ web/lib/Tab/ChangeLog.pm
@@ -2,17 +2,18 @@ package Tab::ChangeLog;
 use base 'Tab::DBI';
 Tab::ChangeLog->table('change_log');
 Tab::ChangeLog->columns(Primary => qw/id/);
-Tab::ChangeLog->columns(Essential => qw/type tourn school person description/);
+Tab::ChangeLog->columns(Essential => qw/type tourn school circuit person description/);
 Tab::ChangeLog->columns(Others => qw/judge entry event category new_panel old_panel fine deleted created timestamp/);
 
-Tab::ChangeLog->has_a(person => "Tab::Person");
-Tab::ChangeLog->has_a(tourn => "Tab::Tourn");
-Tab::ChangeLog->has_a(event => "Tab::Event");
+Tab::ChangeLog->has_a(person   => "Tab::Person");
+Tab::ChangeLog->has_a(tourn    => "Tab::Tourn");
+Tab::ChangeLog->has_a(event    => "Tab::Event");
+Tab::ChangeLog->has_a(circuit  => "Tab::Circuit");
 Tab::ChangeLog->has_a(category => "Tab::Category");
-Tab::ChangeLog->has_a(entry => "Tab::Entry");
-Tab::ChangeLog->has_a(school => "Tab::School");
-Tab::ChangeLog->has_a(judge => "Tab::Judge");
-Tab::ChangeLog->has_a(fine => "Tab::Fine");
+Tab::ChangeLog->has_a(entry    => "Tab::Entry");
+Tab::ChangeLog->has_a(school   => "Tab::School");
+Tab::ChangeLog->has_a(judge    => "Tab::Judge");
+Tab::ChangeLog->has_a(fine     => "Tab::Fine");
 
 Tab::ChangeLog->has_a(new_panel => "Tab::Panel");
 Tab::ChangeLog->has_a(old_panel => "Tab::Panel");
