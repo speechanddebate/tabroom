9267bb2645c6b649ac49d57510000ba3b8c13ff5
Chris Palmer <palmer@tabroom.com>
2020-07-30 17:15:23 -0400

    moved blast to autoqueue.  prioritize autoqueue.  checking on results.

--- web/lib/Tab/Autoqueue.pm
+++ web/lib/Tab/Autoqueue.pm
@@ -2,7 +2,7 @@ package Tab::Autoqueue;
 use base 'Tab::DBI';
 Tab::Autoqueue->table('autoqueue');
 Tab::Autoqueue->columns(Primary => qw/id/);
-Tab::Autoqueue->columns(Essential => qw/tag event round active_at timestamp created_by created_at/);
+Tab::Autoqueue->columns(Essential => qw/tag event round active_at message timestamp created_by created_at/);
 Tab::Autoqueue->has_a(round => 'Tab::Round');
 Tab::Autoqueue->has_a(event => 'Tab::Event');
 Tab::Autoqueue->has_a(created_by => 'Tab::Person');
