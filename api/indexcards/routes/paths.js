c6691dcdacd016a95e6b3634cb9549e7a22a602c
Chris Palmer <palmer@tabroom.com>
2020-12-30 15:21:33 -0500

    Brave new world

---
+++ api/indexcards/routes/paths.js
@@ -0,0 +1,12 @@
+// Utility functions
+import status from '../controllers/utility/status';
+import postError from '../controllers/utility/postError';
+
+// import getPerson from '../controllers/user/getPerson';
+// import getLogin from '../controllers/user/getLogin';
+// { path : '/user/person/' , module : getPerson },
+
+export default [
+    { path : '/status'       , module : status },
+    { path : '/error'       , module : postError },
+];
--- web/lib/Tab/Person.pm
+++ web/lib/Tab/Person.pm
@@ -7,8 +7,7 @@ Tab::Person->columns(Primary   => qw/id/);
 Tab::Person->columns(Essential => qw/email first middle last phone provider site_admin /);
 
 Tab::Person->columns(Others    => qw/street city state zip country postal
-                                   	gender pronoun no_email tz diversity 
-								  	nsda timestamp/);
+                                   	gender pronoun no_email tz nsda timestamp/);
 
 Tab::Person->columns(TEMP => qw/prefs student_id judge_id/);
 
@@ -45,13 +44,13 @@ sub setting {
 	$/ = "";			#Remove all trailing newlines
 	chomp $blob;
 
-	my $existing = Tab::PersonSetting->search(  
+	my $existing = Tab::PersonSetting->search(
 		person => $self->id,
 		tag    => $tag,
 	)->first;
 
-	if (defined $value) { 
-			
+	if (defined $value) {
+
 		if ($existing) {
 
 			$existing->value($value);
@@ -59,7 +58,7 @@ sub setting {
 			$existing->value_date($blob) if $value eq "date";
 			$existing->update;
 
-			if ($value eq "delete" || $value eq "" || $value eq "0") { 
+			if ($value eq "delete" || $value eq "" || $value eq "0") {
 				$existing->delete;
 			}
 
@@ -73,11 +72,11 @@ sub setting {
 				value  => $value,
 			});
 
-			if ($value eq "text") { 
+			if ($value eq "text") {
 				$existing->value_text($blob);
 			}
 
-			if ($value eq "date") { 
+			if ($value eq "date") {
 				$existing->value_date($blob);
 			}
 
@@ -104,54 +103,54 @@ sub all_permissions {
 	my $tourn = shift;
 	my %perms;
 
-	if ($tourn) { 
+	if ($tourn) {
 
 		Tab::Permission->set_sql( tourn_perms => "
 			select permission.*
 			from permission
-			where permission.person = ? 
-			and permission.tourn = ? 
+			where permission.person = ?
+			and permission.tourn = ?
 		");
 
-		my @tourn_perms =  Tab::Permission->search_tourn_perms( 
+		my @tourn_perms =  Tab::Permission->search_tourn_perms(
 			$self->id,
 			$tourn->id
 		);
 
 		PERM:
-		foreach my $perm (@tourn_perms) { 
+		foreach my $perm (@tourn_perms) {
 
 			my $tag = $perm->tag;
 
-			if ($tag eq "contact") { 
+			if ($tag eq "contact") {
 				$perms{$tag} = $perm;
 				next PERM;
 			}
 
-			if ($tag eq "detailed") { 
+			if ($tag eq "detailed") {
 
 				$perms{$tag} = $perm;
 				$perms{"tourn"}{$tourn->id} = $tag;
-				$perms{"details"} = eval { 
+				$perms{"details"} = eval {
 					return JSON::decode_json($perm->details);
 				};
 
-			} else { 
+			} else {
 				$perms{"tourn"}{$tourn->id} = $tag;
 				$perms{"undetailed"}{$tourn->id}++;
 				$perms{$tag} = $perm;
 			}
 		}
 
-		if ($self && $self->site_admin) { 
+		if ($self && $self->site_admin) {
 			$perms{"owner"}++;
 			$perms{"tourn"}{$tourn->id} = "owner";
 			$perms{"undetailed"}{$tourn->id}++;
 			delete $perms{"entry_only"};
 			delete $perms{"details"};
-		}   
-				
-		if ($perms{"undetailed"}{$tourn->id}) { 
+		}
+
+		if ($perms{"undetailed"}{$tourn->id}) {
 			# Universal perms override specific ones
 			delete $perms{"details"};
 			delete $perms{"detailed"};
@@ -162,20 +161,20 @@ sub all_permissions {
 	my $dbh = Tab::DBI->db_Main();
 
     my $sth = $dbh->prepare("
-		select permission.tag, permission.region, permission.circuit, 
+		select permission.tag, permission.region, permission.circuit,
 			permission.chapter, permission.district
 		from permission
-		where permission.person = ? 
+		where permission.person = ?
 			and (permission.tourn is null or permission.tourn = 0)
     ");
-    
-    $sth->execute($self->id);
-    
-    while( 
-		my ($tag, $region, $circuit, $chapter, $district)  = $sth->fetchrow_array() 
-	) { 
 
-		if ($district) { 
+    $sth->execute($self->id);
+
+    while(
+		my ($tag, $region, $circuit, $chapter, $district)  = $sth->fetchrow_array()
+	) {
+
+		if ($district) {
 			$perms{"district"}{$district} = $tag;
 		}
 
@@ -187,7 +186,7 @@ sub all_permissions {
 			$perms{"circuit"}{$circuit} = $tag;
 		}
 
-		if ($chapter) { 
+		if ($chapter) {
 			$perms{"chapter"}{$chapter} = $tag;
 		}
 	}
@@ -198,7 +197,7 @@ sub all_permissions {
 
 }
 
-sub all_settings { 
+sub all_settings {
 
 	my $self = shift;
 
@@ -209,24 +208,24 @@ sub all_settings {
     my $sth = $dbh->prepare("
 		select setting.tag, setting.value, setting.value_date, setting.value_text
 		from person_setting setting
-		where setting.person = ? 
+		where setting.person = ?
         order by setting.tag
     ");
-    
+
     $sth->execute($self->id);
-    
-    while( my ($tag, $value, $value_date, $value_text)  = $sth->fetchrow_array() ) { 
 
-		if ($value eq "date") { 
+    while( my ($tag, $value, $value_date, $value_text)  = $sth->fetchrow_array() ) {
 
-			my $dt = Tab::DBI::dateparse($value_date); 
+		if ($value eq "date") {
+
+			my $dt = Tab::DBI::dateparse($value_date);
 			$all_settings{$tag} = $dt if $dt;
 
-		} elsif ($value eq "text") { 
+		} elsif ($value eq "text") {
 
 			$all_settings{$tag} = $value_text;
 
-		} else { 
+		} else {
 
 			$all_settings{$tag} = $value;
 
