fdac67e35e2af4d51c61e6e15084a95eed940569
Chris Palmer <palmer@tabroom.com>
2020-12-31 13:27:01 -0500

    Lots of models back

---
+++ api/indexcards/models/follower.js
@@ -0,0 +1,26 @@
+/* jshint indent: 4 */
+
+module.exports = function(sequelize, DataTypes) {
+	return sequelize.define('follower', { 
+		type: {
+			type: DataTypes.STRING(8),
+			allowNull: false
+		},
+		cell: {
+			type: DataTypes.INTEGER(20),
+			allowNull: false
+		},
+		domain: {
+			type: DataTypes.STRING(64),
+			allowNull: true
+		},
+		email: {
+			type: DataTypes.STRING(64),
+			allowNull: true
+		}
+	});
+};
+
+
+
+
