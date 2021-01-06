c10a7d7d5a1f9b5108a0dd778c8a4f1019627860
Chris Palmer <palmer@tabroom.com>
2021-01-06 16:42:39 -0500

    Menu better

--- web/lib/Tab/Session.pm
+++ web/lib/Tab/Session.pm
@@ -1,7 +1,7 @@
 package Tab::Session;
 use base 'Tab::DBI';
 Tab::Session->table('session');
-Tab::Session->columns(All    => qw/id person userkey timestamp ip tourn event category weekend su defaults/);
+Tab::Session->columns(All    => qw/id person userkey timestamp ip tourn event category weekend su defaults created_at/);
 Tab::Session->has_a(person   => 'Tab::Person');
 Tab::Session->has_a(su       => 'Tab::Person');
 Tab::Session->has_a(tourn    => 'Tab::Tourn');
@@ -14,23 +14,23 @@ sub account {
 	return $self->person;
 }
 
-__PACKAGE__->_register_datetimes( qw/timestamp/);
+__PACKAGE__->_register_datetimes( qw/timestamp created_at/);
 
-sub default { 
-	
+sub default {
+
 	my ($self, $input) = @_;
-	 
-	if ($input) { 
 
-		my $json = eval{ 
+	if ($input) {
+
+		my $json = eval{
 			return JSON::encode_json($input);
 		};
-	
+
 		$self->defaults($json);
 		$self->update();
 
-	} else { 
-		return eval { 
+	} else {
+		return eval {
 			return JSON::decode_json($self->defaults);
 		};
 	}
