e241534f99d6a79a833a233880b3c4f25ea7f4cc
Chris Palmer <palmer@tabroom.com>
2021-02-22 15:20:01 -0500

    fair overhaul of that mess

--- doc/convert/lib/Tab/Email.pm
+++
@@ -1,9 +0,0 @@
-package Tab::Email;
-use base 'Tab::DBI';
-Tab::Email->table('email');
-Tab::Email->columns(Primary => qw/id/);
-Tab::Email->columns(Essential => qw/timestamp subject league tournament senton sender sent_to blurb/);
-Tab::Email->has_a(sender => 'Tab::Account');
-Tab::Email->has_a(tournament => 'Tab::Tournament');
-__PACKAGE__->_register_datetimes( qw/senton/);
-
--- doc/convert/lib/Tab/JudgeSetting.pm
+++
@@ -1,9 +0,0 @@
-package Tab::JudgeSetting;
-use base 'Tab::DBI';
-Tab::JudgeSetting->table('judge_setting');
-Tab::JudgeSetting->columns(All => qw/id judge tag value value_date value_text timestamp/);
-Tab::JudgeSetting->has_a(judge => 'Tab::Judge');
-
-__PACKAGE__->_register_datetimes( qw/timestamp/);
-__PACKAGE__->_register_datetimes( qw/value_date/);
-
