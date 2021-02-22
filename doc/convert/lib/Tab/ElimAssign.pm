1649d23410871957c583f6e52930b579f54c53b4
Chris Palmer <palmer@tabroom.com>
2021-02-22 15:20:01 -0500

    fair overhaul of that mess

--- doc/convert/lib/Tab/ElimAssign.pm
+++
@@ -1,9 +0,0 @@
-
-package Tab::ElimAssign;
-use base 'Tab::DBI';
-Tab::ElimAssign->table('elim_assign');
-Tab::ElimAssign->columns(Primary => qw/id/);
-Tab::ElimAssign->columns(Essential => qw/timestamp judge judge_group round/);
-Tab::ElimAssign->has_a(judge => "Tab::Judge");
-Tab::ElimAssign->has_a(judge_group => "Tab::JudgeGroup");
-
