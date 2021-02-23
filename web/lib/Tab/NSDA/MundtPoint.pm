3c7df7277a2eff878e81902da4fc35ff0b18c14f
Christopher Palmer <Loki Staging>
2021-02-23 17:46:19 -0600

    unsure

--- web/lib/Tab/NSDA/MundtPoint.pm
+++
@@ -1,7 +0,0 @@
-package Tab::NSDA::MundtPoint;
-use base 'Tab::NSDA::PointsDBI';
-Tab::NSDA::MundtPoint->table('mundt_points');
-Tab::NSDA::MundtPoint->columns(Essential => qw/postID school_id postcode postDate tourn_year points comment user_id/);
-Tab::NSDA::MundtPoint->has_a(school_id => 'Tab::NSDA::School');
-
-__PACKAGE__->_register_dates( qw/postDate/);
