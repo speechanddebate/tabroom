aca4247c018e173a8badf397edfe15b50c0b3eb6
Chris Palmer <palmer@tabroom.com>
2021-02-23 14:21:45 -0500

    unconfirmeds begone

--- api/indexcards/models/housingSlots.js
+++ api/indexcards/models/housingSlots.js
@@ -1,18 +1,17 @@
 /* jshint indent: 4 */
 
 module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('housing_slots', { 
+	return sequelize.define('housingSlots', {
+	table_name: 'housing_slots',
 		night: {
 			type: DataTypes.DATEONLY,
 			allowNull: false,
 			defaultValue: '0000-00-00'
 		},
-		slots: { 
+		slots: {
 			type: DataTypes.INTEGER(6),
 			allowNull: true
 		}
 	});
 };
-
-
 
