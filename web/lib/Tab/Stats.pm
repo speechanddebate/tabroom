557b0242dcd5b149c30731378bce3e56a380ec94
Chris Palmer <palmer@tabroom.com>
2021-02-28 11:12:04 -0500

    Deletions or moves

--- web/lib/Tab/Stats.pm
+++
@@ -1,8 +0,0 @@
-package Tab::Stats;
-use base 'Tab::DBI';
-Tab::Stats->table('stats');
-Tab::Stats->columns(All => qw/id type tag value event timestamp/);
-Tab::Stats->has_a(event => 'Tab::Event');
-
-__PACKAGE__->_register_datetimes( qw/timestamp/);
-
