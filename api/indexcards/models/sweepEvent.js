9b207ab75826ddc860ca17ca909e9a003b18bc40
Chris Palmer <palmer@tabroom.com>
2021-03-10 14:36:55 -0500

    Making Charlie happy

---
+++ api/indexcards/models/sweepEvent.js
@@ -0,0 +1,15 @@
+/* jshint indent: 4 */
+
+module.exports = function(sequelize, DataTypes) {
+	return sequelize.define('sweepEvent', {
+		table: 'sweep_event',
+		event_type: {
+			type: DataTypes.ENUM('all','congress','debate','speech','wsdc','wudc'),
+			allowNull: true
+		},
+		event_level: {
+			type: DataTypes.ENUM('all','open','jv','novice','champ','es-open','es-novice','middle'),
+			allowNull: true
+		}
+	});
+};
