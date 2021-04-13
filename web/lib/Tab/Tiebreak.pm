c7fe98aaa203d95786f0cf8bc437019363764844
Chris Palmer <palmer@tabroom.com>
2021-04-12 23:17:31 -0400

    Opponent record tiebreakers for CHSSA

--- web/lib/Tab/Tiebreak.pm
+++ web/lib/Tab/Tiebreak.pm
@@ -1,11 +1,13 @@
 package Tab::Tiebreak;
 use base 'Tab::DBI';
 Tab::Tiebreak->table('tiebreak');
-Tab::Tiebreak->columns(All => qw/id name child priority truncate truncate_smallest
-									count multiplier violation tiebreak_set
-									highlow highlow_count highlow_threshold count_round
-									timestamp
-								/);
+Tab::Tiebreak->columns(All => qw/id name
+									count count_round
+									truncate truncate_smallest
+									multiplier violation result priority
+									highlow highlow_count highlow_threshold
+									child tiebreak_set
+									timestamp/);
 Tab::Tiebreak->has_a(tiebreak_set => 'Tab::TiebreakSet');
 Tab::Tiebreak->has_a(child => 'Tab::TiebreakSet');
 
