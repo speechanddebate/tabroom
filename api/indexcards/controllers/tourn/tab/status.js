65d8ab9e31cd900a54c10d6a3620c0ef347d59e7
Chris Palmer <palmer@tabroom.com>
2021-05-14 16:39:22 -0400

    dunno

--- api/indexcards/controllers/tourn/tab/status.js
+++ api/indexcards/controllers/tourn/tab/status.js
@@ -1,23 +1,80 @@
 
 export const attendance = {
     GET: async (req, res) => {
+
 		const db = req.db;
-		const tournId = req.params.tourn_id;
 		const op = db.Sequelize.Op
 
-		// Filter out signup options for tournament admins
+		let queryLimit = "";
 
-		let events = await db.event.findAll({
-			where: { tourn: tournId },
-			include : [
-				{ model: db.eventSetting, as: 'Settings',
-					where : {
-						tag: { [op.notLike] : "signup_%"}
-					},
-					required: false
-				}
-			]
-		});
+		if (req.params.timeslot_id && typeof req.params.timeslot_id == "integer") {
+			queryLimit = " where round.timeslot = ".req.params.timeslot_id;
+		if (req.params.round_id && typeof req.params.round_id == "integer") {
+			queryLimit = " where round.id = ".req.params.round_id;
+		} else {
+			return;
+		}
+
+        const attendanceQuery = `
+			select
+				cl.panel, cl.tag, cl.description,
+					CONVERT_TZ(cl.timestamp, '+00:00', tourn.tz),
+				person.id, person.first, person.last
+
+			from panel, campus_log cl, tourn, person, round
+
+			${queryLimit}
+
+				and panel.round = round.id
+				and panel.id = cl.panel
+				and cl.tourn = tourn.id
+				and cl.person = person.id
+
+				and ( exists (
+						select ballot.id
+							from ballot, judge
+						where judge.id = ballot.judge
+							and judge.person = person.id
+							and ballot.panel = panel.id
+					) or exists (
+						select ballot.id
+							from ballot, entry_student es, student
+						where ballot.panel = panel.id
+							and ballot.entry = es.entry
+							and es.student = student.id
+							and student.person = person.id
+					)
+				)
+			order by cl.timestamp
+        `;
+
+		const startQuery = `
+			select
+				ballot.id,  judge.person, panel.id,
+				CONVERT_TZ(ballot.judge_started, '+00:00', tourn.tz),
+				started_by.first, started_by.last
+
+			from (panel, tourn, round, ballot, event, judge)
+
+				left join person started_by on ballot.started_by = started_by.id
+
+			${queryLimit}
+
+				and panel.round = round.id
+				and round.event = event.id
+				and event.tourn = tourn.id
+				and ballot.panel = panel.id
+				and ballot.judge = judge.id
+				and ballot.judge_started > '1900-00-00 00:00:00'
+		`;
+
+        // A raw query to go through the category filter
+        var results = await db.sequelize.query(
+            judgeQuery,
+            { replacements: {tourn: tournId }}
+        );
+
+
 
 		if (events.count < 1) {
 			return res.status(400).json({ message: 'No events found in that tournament' });
