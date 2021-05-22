ebbf79cc1e37efd15d5ba8d4a22b513760c597e5
Chris Palmer <palmer@tabroom.com>
2021-05-21 22:40:25 -0400

    Status API functional.  Next need tests and deploy.

--- web/lib/Tab/CampusLog.pm
+++ web/lib/Tab/CampusLog.pm
@@ -2,14 +2,15 @@ package Tab::CampusLog;
 use base 'Tab::DBI';
 Tab::CampusLog->table('campus_log');
 Tab::CampusLog->columns(Primary => qw/id/);
-Tab::CampusLog->columns(Essential => qw/tag uuid description tourn school person panel entry judge timestamp/);
+Tab::CampusLog->columns(Essential => qw/tag uuid description tourn school person panel entry judge student timestamp/);
 
-Tab::CampusLog->has_a(person => "Tab::Person");
-Tab::CampusLog->has_a(panel  => "Tab::Panel");
-Tab::CampusLog->has_a(tourn  => "Tab::Tourn");
-Tab::CampusLog->has_a(school => "Tab::School");
-Tab::CampusLog->has_a(entry  => "Tab::Entry");
-Tab::CampusLog->has_a(judge  => "Tab::Judge");
+Tab::CampusLog->has_a(person  => "Tab::Person");
+Tab::CampusLog->has_a(panel   => "Tab::Panel");
+Tab::CampusLog->has_a(tourn   => "Tab::Tourn");
+Tab::CampusLog->has_a(school  => "Tab::School");
+Tab::CampusLog->has_a(student => "Tab::Student");
+Tab::CampusLog->has_a(entry   => "Tab::Entry");
+Tab::CampusLog->has_a(judge   => "Tab::Judge");
 
 __PACKAGE__->_register_datetimes( qw/timestamp/);
 
