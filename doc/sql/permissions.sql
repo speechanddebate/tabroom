f78276cf86b646aa5e39a1dd9f835ac7ab261745
Chris Palmer <palmer@tabroom.com>
2021-08-23 20:07:52 -0400

    Preliminary access changes. Mostly testing vscode checkin

---
+++ doc/sql/permissions.sql
@@ -0,0 +1,11 @@
+
+	update permission set tag = "tabber" where tag  = "full_admin";
+
+	update permission set tag = "checker" where tag = "entry_only";
+	update permission set tag = "checker" where tag = "limited";
+	update permission set tag = "checker" where tag = "category_tab";
+	update permission set tag = "checker" where tag = "registration";
+	update permission set tag = "checker" where tag = "tabbing";
+
+	update permission set tag = "by_event" where tag  = "detailed";
+
