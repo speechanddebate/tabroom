292b1e78c661e535e375e5f026db1f44a399cbeb
Chris Palmer <palmer@tabroom.com>
2021-11-12 14:12:17 -0500

    Test coverage for status board, user profile and location

---
+++ api/indexcards/controllers/user/account/getProfile.test.js
@@ -0,0 +1,62 @@
+import request from 'supertest';
+import { assert } from 'chai';
+import config from '../../../../config/config.js';
+import db from '../../../models/index.cjs';
+import server from '../../../../app.js';
+import userData from '../../../tests/users.js';
+
+describe('User Profile Loader', () => {
+
+	let testAdmin = {};
+	let testAdminSession = {};
+
+	before('Set Dummy Data', async () => {
+		testAdmin = await db.person.create(userData.testAdmin);
+		testAdminSession = await db.session.create(userData.testAdminSession);
+	});
+
+	it('Returns correct JSON for a self profile request', async () => {
+
+		const res = await request(server)
+			.get(`/v1/user/profile`)
+			.set('Accept', 'application/json')
+			.set('Cookie', [`${config.COOKIE_NAME}=${testAdminSession.userkey}`])
+			.expect('Content-Type', /json/)
+			.expect(200);
+
+		assert.isObject(res.body, 'Response is an object');
+		
+		assert.equal(
+			res.body.email,
+			'i.am.god@speechanddebate.org',
+			'Correct fake user profile is returned'
+		);
+		
+		assert.isTrue(res.body.site_admin, 'Site Admin powers are enabled');
+
+	});
+
+	it('Returns correct JSON for another user profile request', async () => {
+
+		const res = await request(server)
+			.get(`/v1/user/profile/1`)
+			.set('Accept', 'application/json')
+			.set('Cookie', [`${config.COOKIE_NAME}=${testAdminSession.userkey}`])
+			.expect('Content-Type', /json/)
+			.expect(200);
+
+		assert.isObject(res.body, 'Response is an object');
+
+		assert.equal(
+			res.body.email,
+			'palmer@tabroom.com', 
+			'Chris Palmer is user number 1'
+		);
+	});
+
+	after('Remove Dummy Data', async () => {
+		await testAdminSession.destroy();
+		await testAdmin.destroy();
+	});
+
+});
---
+++ api/indexcards/controllers/user/account/ipLocation.test.js
@@ -0,0 +1,32 @@
+import request from 'supertest';
+import { assert } from 'chai';
+import config from '../../../../config/config.js';
+import db from '../../../models/index.cjs';
+import server from '../../../../app.js';
+
+describe('IP Location Results', () => {
+
+	it('Returns correct location data', async () => {
+
+		const res = await request(server)
+			.get(`/v1/user/iplocation/1.1.1.1`)
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(200);
+
+		assert.isObject(res.body, 'Response is an object');
+		
+		assert.equal(
+			res.body.country,
+			'Australia',
+			'Location is in Oz as it should be'
+		);
+		
+		assert.isFalse(
+			res.body.isEU, 
+			'Australia may be in Eurovision but it is not in the EU.  But God if it joined? That would SERIOUSLY piss off the Brits.  For it'
+		);
+
+	});
+
+});
