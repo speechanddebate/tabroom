6ca3283fbf4b803669d7950da4df397c0d059b8e
Chris Palmer <palmer@tabroom.com>
2022-01-15 17:57:59 -0500

    Public paradigm forms answers

--- web/lib/Tab/Quiz.pm
+++ web/lib/Tab/Quiz.pm
@@ -3,7 +3,7 @@ use base 'Tab::DBI';
 Tab::Quiz->table('quiz');
 Tab::Quiz->columns(Primary => qw/id/);
 Tab::Quiz->columns(Essential => qw/tag label questions description hidden
-		badge badge_link badge_description
+		badge badge_link badge_description show_answers
 		created_at timestamp/);
 Tab::Quiz->columns(Others => qw/sitewide approval admin_only person circuit tourn/);
 
