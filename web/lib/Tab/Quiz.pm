43f2dd59fad3544d0aa493c594dc8763adb6d941
Chris Palmer <palmer@tabroom.com>
2022-02-26 19:41:47 -0500

    NSDA and TOC questionnaires for judges

--- web/lib/Tab/Quiz.pm
+++ web/lib/Tab/Quiz.pm
@@ -7,9 +7,10 @@ Tab::Quiz->columns(Essential => qw/tag label questions description hidden
 		created_at timestamp/);
 Tab::Quiz->columns(Others => qw/sitewide approval admin_only person circuit tourn/);
 
-Tab::Quiz->has_a(person  => 'Tab::Person');
-Tab::Quiz->has_a(tourn   => 'Tab::Tourn');
-Tab::Quiz->has_a(circuit => 'Tab::Circuit');
+Tab::Quiz->has_a(person     => 'Tab::Person');
+Tab::Quiz->has_many(answers => 'Tab::PersonQuiz');
+Tab::Quiz->has_a(tourn      => 'Tab::Tourn');
+Tab::Quiz->has_a(circuit    => 'Tab::Circuit');
 
 Tab::Quiz->has_many(takers => [Tab::PersonQuiz => 'person']);
 
