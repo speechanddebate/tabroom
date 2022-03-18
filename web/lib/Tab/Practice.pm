854700a06549b0c9dbb9ef001ccff8dce7fde4fa
Chris Palmer <palmer@tabroom.com>
2022-03-17 20:11:30 -0400

    woosh

---
+++ web/lib/Tab/Practice.pm
@@ -0,0 +1,9 @@
+package Tab::Practice;
+use base 'Tab::DBI';
+Tab::Practice->table('practice');
+Tab::Practice->columns(Essential => qw/id name start end reported created_at created_by chapter timestamp /);
+Tab::Practice->has_a(chapter => 'Tab::Chapter');
+Tab::Practice->has_a(created_by => 'Tab::Person');
+Tab::Practice->has_many(students => [Tab::PracticeStudent => 'student']);
+__PACKAGE__->_register_datetimes( qw/start end created_at timestamp/);
+
