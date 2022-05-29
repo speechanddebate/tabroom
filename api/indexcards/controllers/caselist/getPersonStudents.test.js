dd7163d44dbadd7ca4c925c2477cffcdb8b1f06d
Aaron Hardy <ashtarcommunications@gmail.com>
2022-05-29 15:07:06 -0600

    Caselist endpoints for Hardy

---
+++ api/indexcards/controllers/caselist/getPersonStudents.test.js
@@ -0,0 +1,34 @@
+import crypto from 'crypto';
+import request from 'supertest';
+import { assert } from 'chai';
+import config from '../../../config/config.js';
+import db from '../../models/index.cjs';
+import server from '../../../app.js';
+import userData from '../../tests/users.js';
+
+describe('Person Students', () => {
+	let testAdmin = {};
+	let testAdminSession = {};
+
+	before('Set Dummy Data', async () => {
+		testAdmin = await db.person.create(userData.testAdmin);
+		testAdminSession = await db.session.create(userData.testAdminSession);
+	});
+
+	it('Returns students for a person', async () => {
+        const hash = crypto.createHash('sha256').update(config.CASELIST_KEY).digest('hex');
+		const res = await request(server)
+			.get(`/v1/caselist/students?person_id=17145&caselist_key=${hash}`)
+			.set('Accept', 'application/json')
+			.set('Cookie', [`${config.COOKIE_NAME}=${testAdminSession.userkey}`])
+			.expect('Content-Type', /json/)
+			.expect(200);
+
+		assert.isArray(res.body, 'Response is an array');
+	});
+
+	after('Remove Dummy Data', async () => {
+		await testAdminSession.destroy();
+		await testAdmin.destroy();
+	});
+});
--- api/indexcards/routes/definitions/schemas/index.js
+++ api/indexcards/routes/definitions/schemas/index.js
@@ -3,7 +3,11 @@ import Person from './Person.js';
 import Login from './Login.js';
 import School from './School.js';
 import SchoolSetting from './SchoolSetting.js';
+import Chapter from './Chapter.js';
 import Event from './Event.js';
+import Round from './Round.js';
+import Student from './Student.js';
+import CaselistLink from './CaselistLink.js';
 
 export default {
 	Err,
@@ -11,5 +15,9 @@ export default {
 	Login,
 	School,
 	SchoolSetting,
+    Chapter,
 	Event,
+	Round,
+	Student,
+    CaselistLink
 };
