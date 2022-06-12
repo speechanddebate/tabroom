49cdb240c77468f6af907889b159833e517c2540
Aaron Hardy <ashtarcommunications@gmail.com>
2022-06-12 11:58:39 -0600

    Oops, old files

--- api/model.mjs
+++
@@ -1,9 +0,0 @@
-import obj from 'objection-swagger';
-import models from './models.js';
-
-const { saveSchema } = obj;
-const main = async () => {
-    saveSchema(models, './schemas');
-}
-
-main();
