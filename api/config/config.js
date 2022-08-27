b2684f66d9b1e7fd0fe653be3159dad524fe26df
Chris Palmer <palmer@tabroom.com>
2022-08-27 13:36:01 -0400

    A whole lotta tests.  My brain is goo

--- api/config/config.js
+++ api/config/config.js
@@ -1,3 +1,4 @@
+// eslint-disable import/no-unresolved
 import thisShouldNotBeNecessary from './config.cjs';
 
 export default thisShouldNotBeNecessary;
--- api/indexcards/controllers/caselist/getPersonRounds.test.js
+++ api/indexcards/controllers/caselist/getPersonRounds.test.js
@@ -4,17 +4,9 @@ import { assert } from 'chai';
 import config from '../../../config/config.js';
 import db from '../../models/index.cjs';
 import server from '../../../app.js';
-import userData from '../../../tests/users.js';
+import { testAdminSession } from '../../../tests/testFixtures.js';
 
 describe('Person Rounds', () => {
-	let testAdmin = {};
-	let testAdminSession = {};
-
-	beforeAll(async () => {
-		testAdmin = await db.person.create(userData.testAdmin);
-		testAdminSession = await db.session.create(userData.testAdminSession);
-	});
-
 	it('Returns rounds for a person', async () => {
 		const hash = crypto.createHash('sha256').update(config.CASELIST_KEY).digest('hex');
 		const res = await request(server)
@@ -40,13 +32,5 @@ describe('Person Rounds', () => {
 			.expect(200);
 
 		assert.isArray(res.body, 'Response is an array');
-	});
-
-	afterAll(async () => {
-		await db.sequelize.query(`
-            DELETE FROM caselist WHERE person = 17145
-        `);
-		await testAdminSession.destroy();
-		await testAdmin.destroy();
 	});
 });
