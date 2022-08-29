714662b1bab744ea89a74abf937295f33590516c
Chris Palmer <palmer@tabroom.com>
2022-08-29 10:51:33 -0400

    App changed a titch

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { unstable_HistoryRouter as Router, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { createBrowserHistory } from 'history';
 
 import store from './redux/store';
 import ErrorBoundary from './ErrorBoundary';
@@ -8,13 +9,17 @@ import Layout from './Layout';
 import TestTable from './TestTable';
 
 const App = () => {
+	const history = createBrowserHistory({ window });
+
 	return (
-		<Router>
+		<Router history={history}>
 			<Provider store={store}>
 				<ErrorBoundary>
 					<Routes>
 						<Route exact path="/" element={<Layout><TestTable /></Layout>} />
-						<Route exact path="/route" element={<Layout>{() => <p>I&apos;m a component, hear me roar!</p>}</Layout>} />
+						<Route exact path="/route/:name" element={<Layout><TestTable /></Layout>} />
+						<Route exact path="/route" element={<Layout><p>I&apos;m a component, hear me roar!</p></Layout>} />
+						<Route path="*" element={<Layout><p>I am 404.  Hear me roar.</p></Layout>} />
 					</Routes>
 				</ErrorBoundary>
 			</Provider>
