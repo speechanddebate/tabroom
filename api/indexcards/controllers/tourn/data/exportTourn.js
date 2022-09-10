2c192a34789e41214f13293bef5c957b9f1d8ec3
Chris Palmer <palmer@tabroom.com>
2022-09-10 12:27:01 -0400

    Trafner

---
+++ api/indexcards/controllers/tourn/data/exportTourn.js
@@ -0,0 +1,30 @@
+//import { showDateTime } from '../../../helpers/common';
+
+export const backupTourn = {
+	GET: async (req, res) => {
+		const db = req.db;
+		const tournId = req.params.tourn_id;
+
+		const tourn = await db.tourn.findByPk(tournId);
+		return res.status(200).json(tourn);
+	},
+};
+
+export const restoreTourn = {
+	POST: async (req, res) => {
+
+		const tournData = req.body;
+
+		if (!tournData.tourn_id) { 
+			tournData.tourn_id = req.params.tourn_id;
+		}
+
+		if (!tournData.tourn_id) { 
+			tournData.tourn_id = req.session.tourn_id;
+		}
+
+		// process it later but for now
+
+		return res.status(200).json({"message": "yay"});
+	},
+};
