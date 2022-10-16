1fec9a8dfe7d5c1cda344832b3b1a759d98c8749
Chris Palmer <palmer@tabroom.com>
2022-10-16 11:43:37 -0400

    It's the worst version of tabroom I've ever heard of.  But you have heard of me.

---
+++ client/src/common/NavMenu.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import styles from './header.module.css';
+
+export const NavMenu = () => {
+
+	const sector = useSelector((state) => state.sector);
+
+	if (sector === 'tab') {
+
+		// Tournament Administrator Menu
+
+	} else if (sector === 'user') {
+
+		// User Registration Menu
+
+	} else {
+
+		return (
+			<ul id={styles.nav}>
+				<li class={styles.top}>
+					<a class={styles.top_link} href="/">
+						<span class={styles.down}>
+							Home
+						</span>
+					</a>
+				</li>
+
+				<li class={styles.top}>
+					<a class={styles.top_link} href="/index/circuits.mhtml">
+						<span class={styles.down}>
+							Circuits
+						</span>
+					</a>
+				</li>
+
+				<li class={styles.top}>
+					<a class={styles.top_link} href="/index/results/">
+						<span class={styles.down}>
+							Results
+						</span>
+					</a>
+				</li>
+
+				<li class={styles.top}>
+					<a class={styles.top_link} href="/index/paradigm.mhtml">
+						<span class={styles.down}>
+							Paradigms
+						</span>
+					</a>
+				</li>
+
+				<li class={styles.top}>
+					<a class={styles.top_link} href="/index/help.mhtml">
+						<span class={styles.down}>
+							Help
+						</span>
+					</a>
+				</li>
+
+				<li class={styles.top}>
+					<a class={styles.top_link} href="/index/about.mhtml">
+						<span class={styles.down}>
+							About
+						</span>
+					</a>
+				</li>
+			</ul>
+		);
+	}
+};
+
+export default NavMenu;
---
+++ client/src/common/SearchBar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import styles from './header.module.css';
+
+const SearchBar = () => {
+
+	const sector = useSelector((state) => state.sector);
+	const tourn = useSelector((state) => state.tourn);
+	const setupSearch = {};
+
+	if (sector === 'tab') {
+		setupSearch.api = `/tourn/${tourn}/search`;
+		setupSearch.tag = 'Search Attendees';
+	} else {
+		setupSearch.api = `/public/search`;
+		setupSearch.tag = 'Search Tournaments';
+	}
+
+	// Presumably here I can dispatch some task to send the search term to an
+	// API and then display a results listing.  Gotta build said API first.
+
+	// Which isn't such a bad next step...
+
+	return (
+		<span id={styles.search} title={setupSearch.tag}>
+			<input
+				id             = {styles.searchtext}
+				type           = "text"
+				maxLength      = "128"
+				name           = "search"
+				caller         = "home"
+				placeholder    = {setupSearch.tag}
+				className      = "notfirst"
+				tabIndex       = "-1"
+				autoComplete   = "off"
+				autoCorrect    = "off"
+				autoCapitalize = "off"
+				spellCheck     = "false"
+			/>
+			<button type="submit" className={styles.searchbutton}>
+				<img alt="Search" src="/images/search.png" />
+			</button>
+		</span>
+	);
+};
+
+export default SearchBar;
