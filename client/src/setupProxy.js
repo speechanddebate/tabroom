b5ee398e5d3569adf48a2d92c161f689a7e80493
Chris Palmer <palmer@tabroom.com>
2022-11-11 13:31:33 -0500

    So so broken

---
+++ client/src/setupProxy.js
@@ -0,0 +1,12 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+	console.log(process.env.REACT_APP_PROXY_HOST);
+	app.use(
+		'/v1',
+		createProxyMiddleware({
+			target: process.env.REACT_APP_PROXY_HOST,
+			changeOrigin: true,
+		})
+	);
+};
