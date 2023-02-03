cd2aad17ecfeaa7f678bfc22ec90832510be0e17
Chris Palmer <palmer@tabroom.com>
2023-02-03 18:28:48 -0500

    I shall be cursed unto the 9th generation for what I have done here

--- web/lib/Tab/Fine.pm
+++ web/lib/Tab/Fine.pm
@@ -2,12 +2,13 @@ package Tab::Fine;
 use base 'Tab::DBI';
 Tab::Fine->table('fine');
 Tab::Fine->columns(All => qw/id reason amount payment
-								deleted deleted_at deleted_by levied_at levied_by 
-								tourn school region judge parent invoice timestamp/);
+								deleted deleted_at deleted_by levied_at levied_by
+								tourn school region judge person parent invoice timestamp/);
 
 Tab::Fine->columns(TEMP => qw/schoolid regionid judgeid/);
 
 Tab::Fine->has_a(judge => 'Tab::Judge');
+Tab::Fine->has_a(person => 'Tab::Person');
 Tab::Fine->has_a(tourn => 'Tab::Tourn');
 Tab::Fine->has_a(parent => 'Tab::Fine');
 Tab::Fine->has_a(invoice => 'Tab::Invoice');
