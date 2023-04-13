410fbbf53765728c39b5d1adb5d95434c0577000
Chris Palmer <palmer@tabroom.com>
2023-04-13 17:14:31 -0400

    after this put down the nitpicky bullshit palmer

--- api/indexcards/controllers/tourn/section/blast/message.js
+++ api/indexcards/controllers/tourn/section/blast/message.js
@@ -20,6 +20,7 @@ export const messageSection = {
 		);
 
 		messageData.text = `\n ${req.body.message}`;
+		messageData.html = `<p>${req.body.message}</p>`;
 		messageData.subject = 'Message from Tab';
 		const emailResponse = await emailBlast(messageData);
 		const phoneResponse = await phoneBlast(messageData);
@@ -57,6 +58,7 @@ export const messageRound = {
 		);
 
 		messageData.text = `\n ${req.body.message}`;
+		messageData.html = `<p>${req.body.message}</p>`;
 		messageData.subject = 'Message from Tab';
 		const emailResponse = await emailBlast(messageData);
 		const phoneResponse = await phoneBlast(messageData);
@@ -94,6 +96,7 @@ export const messageTimeslot = {
 		);
 
 		messageData.text = `\n ${req.body.message}`;
+		messageData.html = `<p>${req.body.message}</p>`;
 		messageData.subject = 'Message from Tab';
 		const emailResponse = await emailBlast(messageData);
 		const phoneResponse = await phoneBlast(messageData);
