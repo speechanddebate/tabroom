6ae53963fbef6df32150e431127e5bd06634007b
Chris Palmer <palmer@tabroom.com>
2023-06-06 23:34:00 -0400

    Status log from node pulling soon

--- api/indexcards/routes/paths/tourn/round.js
+++ api/indexcards/routes/paths/tourn/round.js
@@ -5,9 +5,11 @@
 import { roundJudges } from '../../../controllers/tourn/section/cleanJudges';
 import { messageRound } from '../../../controllers/tourn/section/blast/message.js';
 import { blastRound } from '../../../controllers/tourn/section/blast/pairing.js';
+import { getRoundLog } from '../../../controllers/tourn/schematic/changeLog.js';
 
 export default [
 	{ path : '/tourn/{tourn_id}/round/{round_id}/judges'  , module : roundJudges }  ,
 	{ path : '/tourn/{tourn_id}/round/{round_id}/message' , module : messageRound } ,
 	{ path : '/tourn/{tourn_id}/round/{round_id}/blast'   , module : blastRound }   ,
+	{ path : '/tourn/{tourn_id}/round/{round_id}/log'     , module : getRoundLog }  ,
 ];
