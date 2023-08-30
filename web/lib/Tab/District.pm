23a31e870816740e3c20e2201cb56aef3a1b18fa
Chris Palmer <palmer@tabroom.com>
2023-08-30 17:26:34 -0400

    Did not bother

--- web/lib/Tab/District.pm
+++ web/lib/Tab/District.pm
@@ -3,7 +3,7 @@ use base 'Tab::DBI';
 Tab::District->table('district');
 Tab::District->columns(Primary => qw/id/);
 Tab::District->columns(Essential => qw/name code location timestamp/);
-Tab::District->columns(Others => qw/level realm financials/);
+Tab::District->columns(Others => qw/level realm/);
 
 Tab::District->has_many(chapters => 'Tab::Chapter', 'district');
 Tab::District->has_many(schools => 'Tab::School', 'district');
@@ -11,19 +11,3 @@ Tab::District->has_many(schools => 'Tab::School', 'district');
 Tab::District->has_many(permissions => 'Tab::Permission', 'district');
 Tab::District->has_many(admins => [ Tab::Permission => 'person']);
 
-sub financial {
-	my ($self, $input) = @_;
-
-	if ($input) {
-		my $json = eval{
-			return JSON::encode_json($input);
-		};
-
-		$self->financials($json);
-		$self->update();
-	} else {
-		return eval {
-			return JSON::decode_json($self->financials);
-		};
-	}
-}
