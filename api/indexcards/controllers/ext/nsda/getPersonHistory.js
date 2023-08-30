cb85b2de6b88911c6ed16cf516a2deffd8799807
Aaron Hardy <ashtarcommunications@gmail.com>
2023-08-30 16:06:51 -0600

    It'll work for now so I can test things

---
+++ api/indexcards/controllers/ext/nsda/getPersonHistory.js
@@ -0,0 +1,104 @@
+import crypto from 'crypto';
+import config from '../../../../config/config';
+
+export const getPersonHistory = {
+	GET: async (req, res) => {
+		const db = req.db;
+		const hash = crypto.createHash('sha256').update(config.NSDA_KEY).digest('hex');
+		if (req.query.nsda_key !== hash) {
+			return res.status(401).json({ message: 'Invalid NSDA key' });
+		}
+
+		const personId = await db.sequelize.query(`
+			SELECT DISTINCT P.id FROM person P
+			LEFT JOIN student S ON S.person = P.id
+			LEFT JOIN chapter_judge CJ ON CJ.person = P.id
+			WHERE P.nsda = ? OR S.nsda = ? OR CJ.nsda = ?
+		`, { replacements: [req.query.person_id, req.query.person_id, req.query.person_id] });
+
+		const student = await db.sequelize.query(`
+			SELECT
+				T.name AS 'tournament',
+				T.state as 'state',
+				T.start AS 'start',
+				T.end AS 'end',
+				C.name AS 'chapter',
+				EV.name AS 'event'
+			FROM tourn T
+			INNER JOIN school S ON S.tourn = T.id
+			INNER JOIN entry E ON E.school = S.id
+			INNER JOIN event EV ON EV.id = E.event
+			INNER JOIN entry_student ES ON ES.entry = E.id
+			INNER JOIN student ST ON ST.id = ES.student
+			INNER JOIN chapter C ON C.id = ST.chapter
+			WHERE ST.nsda = ?
+				AND T.hidden = 0
+			GROUP BY T.id
+		`, { replacements: [req.query.person_id] });
+
+		const judge = await db.sequelize.query(`
+			SELECT
+				T.name AS 'tournament',
+				T.state as 'state',
+				T.start AS 'start',
+				T.end AS 'end',
+				C.name AS 'chapter',
+				CAT.name AS 'category'
+			FROM tourn T
+			INNER JOIN school S ON S.tourn = T.id
+			INNER JOIN judge J ON J.school = S.id
+			INNER JOIN category CAT ON CAT.id = J.category
+			INNER JOIN chapter_judge CJ ON CJ.id = J.chapter_judge
+			INNER JOIN chapter C ON C.id = CJ.chapter
+			WHERE CJ.nsda = ?
+				AND T.hidden = 0
+			GROUP BY T.id
+		`, { replacements: [req.query.person_id] });
+
+		const history = {
+			personId: personId[0][0].id,
+			student: student[0],
+			judge: judge[0],
+		};
+
+		return res.status(200).json(history);
+	},
+};
+
+getPersonHistory.GET.apiDoc = {
+	summary: 'Load history for a person ID',
+	operationId: 'getPersonHistory',
+	parameters: [
+		{
+			in          : 'query',
+			name        : 'person_id',
+			description : 'ID of person whose history you wish to access',
+			required    : true,
+			schema      : {
+				type    : 'integer',
+				minimum : 1,
+			},
+		},
+		{
+			in          : 'query',
+			name        : 'nsda_key',
+			description : 'Key for API access',
+			required    : true,
+			schema      : {
+				type    : 'string',
+			},
+		},
+	],
+	responses: {
+		200: {
+			description: 'Person History',
+			content: {
+				'*/*': { schema: { type: 'object' } },
+			},
+		},
+		default: { $ref: '#/components/responses/ErrorResponse' },
+	},
+	tags: ['nsda'],
+};
+
+export default getPersonHistory;
--- api/indexcards/routes/paths/ext/nsda.js
+++ api/indexcards/routes/paths/ext/nsda.js
@@ -1,10 +1,12 @@
 // Integrations with NSDA gateways, payments and services that require share keys
 
 // Router controllers
+import getPersonHistory from '../../../controllers/ext/nsda/getPersonHistory';
 import { postPayment } from '../../../controllers/ext/nsda/payment';
 import { syncNatsAppearances } from '../../../controllers/ext/nsda/natsAppearances';
 
 export default [
+	{ path : '/nsda/history'            , module : getPersonHistory },
 	{ path : '/nsda/payment'            , module : postPayment },
 	{ path : '/nsda/payment/{tourn_id}' , module : postPayment },
 	{ path : '/nsda/nats-appearances'   , module : syncNatsAppearances },
