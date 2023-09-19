5f5d37a87b6e91b5eec66e59c2c9fa7f310f96a6
Chris Palmer <palmer@tabroom.com>
2023-09-19 12:22:08 -0400

    tournList

--- client/src/public/TournList.js
+++ client/src/public/TournList.js
@@ -23,17 +23,25 @@ const TournList = () => {
 		fetchList();
 	}, []);
 
-	const tournTableColumns = [
+	const tableColumns = [
 		{
 			header   : 'Dates',
 			style    : 'nowrap',
 			sortKey  : 'week',
-			accessor : (tourn) => {
+			accessor : (row) => {
+				const tourn = row?.original;
+				if (!tourn) {
+					return '';
+				}
 				const start = moment(tourn.start, 'YYYY-MM-DDTHH:mm:ssZ');
 				const end = moment(tourn.end, 'YYYY-MM-DDTHH:mm:ssZ');
 				return `${start.tz(tourn.tz).format('YYYY-MM-DD')}-${end.tz(tourn.tz).format('YYYY-MM-DD')}`;
 			},
-			cell    : (tourn) => {
+			cell    : (row) => {
+				const tourn = row?.original;
+				if (!tourn) {
+					return '';
+				}
 				const start = moment(tourn.start, 'YYYY-MM-DDTHH:mm:ssZ');
 				const end = moment(tourn.end, 'YYYY-MM-DDTHH:mm:ssZ');
 
@@ -45,32 +53,28 @@ const TournList = () => {
 				}
 				return '';
 			},
+			sortingFn: (a, b) => {
+				if (a?.original?.week === b?.original?.week) { return 0; }
+				if (a?.original?.week > b?.original?.week) { return 1; }
+				if (a?.original?.week < b?.original?.week) { return -1; }
+				return 0;
+			},
 		},
 		{
-			header : 'Tournament',
-			key    : 'name',
-			cell   : (tourn) => {
-
+			header   : 'Tournament',
+			accessor : 'name',
+			cell     : (row) => {
+				const tourn = row?.original;
+				if (!tourn) {
+					return '';
+				}
 				let tournName = tourn.name;
-
 				if (tourn.district) {
-
-					tournName.replaceAll('District Tournament', ''); 
-
+					tournName.replaceAll('District Tournament', '');
 					tournName = `
-						<span class='semibold half'>
-								NSDA: ${ $tourns{$tourn_id}{'name'} %>
-							</span>
-							<span class='half'>
-								<% $tourns{$tourn_id}{'weekend_name'} %>
-							</span>
-
-				}
-
-				if (tourn.webname) {
-					return `<a href='https://${tourn.webname}.tabroom.com'>${tourn.name}</a>`;
-				}
-				return `<a href='https://www.tabroom.com/index/tourn/index.mhtml?tourn_id=${tourn.id}'>${tourn.name}</a>`;
+						<span class='semibold half'>NSDA: ${tournName}</span>
+						<span class='half'>${tourn.weekendName}</span>
+					`;
 				}
 				if (tourn.webname) {
 					return `<a href='https://${tourn.webname}.tabroom.com'>${tourn.name}</a>`;
@@ -78,15 +82,21 @@ const TournList = () => {
 				return `<a href='https://www.tabroom.com/index/tourn/index.mhtml?tourn_id=${tourn.id}'>${tourn.name}</a>`;
 			},
 		},
-		{ key : 'city' },
-		{ key : 'state' },
+		{ accessor : 'city' },
+		{ accessor : 'state' },
 		{
 			header   : 'Format',
 			style    : 'nowrap centeralign',
-			accessor : (tourn) => {
-				return `${tourn.inp ? 'In Person ' : ''} ${tourn.online ? 'Online' : ''} ${tourn.hybrid ? 'Hybrid' : ''}`;
+			accessor : (row) => {
+				return `${row?.original?.inp ? 'In Person ' : ''} 
+					${row?.original?.online ? 'Online' : ''}
+					${row?.original?.hybrid ? 'Hybrid' : ''}`;
 			},
-			cell     : (tourn) => {
+			cell     : (row) => {
+				const tourn = row?.original;
+				if (!tourn) {
+					return '';
+				}
 				return (
 					<>
 						{
@@ -107,7 +117,11 @@ const TournList = () => {
 		},
 		{
 			header: 'Registration',
-			accessor: (tourn) => {
+			accessor: (row) => {
+				const tourn = row?.original;
+				if (!tourn) {
+					return '';
+				}
 				const regStart = moment(tourn.start, 'YYYY-MM-DDTHH:mm:ssZ');
 				const regEnd = moment(tourn.end, 'YYYY-MM-DDTHH:mm:ssZ');
 				const now = moment();
@@ -125,120 +139,8 @@ const TournList = () => {
 			},
 		},
 		{
-			header : 'Judging Signups In',
-			key    : 'signup',
-		},
-	];
-
-	const tournTable = tourns.map( (tourn) => {
-
-		const tournRow = {};
-
-		['districts', 'nats', 'msnats', 'name'].forEach( (key) => {
-			if (tourn[key]) {
-				tournRow.style = 'semibold';
-			}
-		});
-
-		const start = moment(tourn.start, 'YYYY-MM-DDTHH:mm:ssZ');
-		const end = moment(tourn.end, 'YYYY-MM-DDTHH:mm:ssZ');
-
-		if (start.isValid() && end.isValid()) {
-			if (start.tz(tourn.tz).date() === end.tz(tourn.tz).date()) {
-				tournRow.dates = start.tz(tourn.tz).format('M/D');
-			} else {
-				tournRow.dates = `${start.tz(tourn.tz).format('M/D')} - ${end.tz(tourn.tz).format('M/D')}`;
-			}
-		} else {
-			tournRow.dates = '';
-		}
-
-		if (tourn.webname) {
-			tournRow.link = `https://${tourn.webname}.tabroom.com`;
-		} else if (!tourn.link) {
-			tournRow.link = `https://www.tabroom.com/index/tourn/index.mhtml?tourn_id=${tourn.id}`;
-		}
-
-		tournRow.tournament = tourn.name;
-		tournRow.city = tourn.location;
-
-		if (tourn.country && tourn.country !== 'US') {
-			tournRow.state = tourn.country;
-		} else {
-			tournRow.state = tourn.state;
-		}
-
-		tournRow.type = {};
-		['inp', 'hybrid', 'online'].forEach( (key) => {
-			if (tourn[key] > 0) {
-				tournRow.type[key] = true;
-			}
-		});
-
-		const regStart = moment(tourn.start, 'YYYY-MM-DDTHH:mm:ssZ');
-		const regEnd = moment(tourn.end, 'YYYY-MM-DDTHH:mm:ssZ');
-		if (regStart.isValid() && regEnd.isValid()) {
-			if (now < regStart) {
-				tournRow.registration = `Opens on ${regStart.tz(tourn.tz).format('M/D hh:mm A z')}`;
-			} else if (now < regEnd) {
-				tournRow.registration = `Due by ${regEnd.tz(tourn.tz).format('M/D hh:mm A z')}`;
-			} else {
-				tournRow.registration = 'Closed';
-			}
-		} else {
-			tournRow.registration = '';
-		}
-
-		tournRow.week = tourn.week;
-		tournRow.signup = tourn.signup;
-
-		return tournRow;
-	});
-
-	const transforms = [
-		{ accessor : 'link'  , hidden : true , excludeCSV: true },
-		{ accessor : 'style' , hidden : true , excludeCSV: true },
-		{
-			accessor : 'tournament',
-			cell: (incoming) => {
-				return <a href={`${incoming?.row?.original?.link}`}>{incoming.getValue()}</a>;
-			},
-		},
-		{
-			accessor : 'state',
-			style    : 'centeralign',
-		},
-		{
-			accessor : 'type',
-			style    : 'centeralign',
-			cell: (incoming) => {
-				return (
-					<>
-						{
-							incoming?.getValue().inp &&
-							<span className='third fa fa-sm fa-user bluetext hover' title='Tournament has in-person events' />
-						}
-						{
-							incoming?.getValue().online &&
-							<span className='third fa fa-sm fa-laptop greentext hover' title='Tournament has online events' />
-						}
-						{
-							incoming?.getValue().hybrid &&
-							<span className='third fa fa-sm fa-handshake-o orangetext hover' title='Tournament has hybrid events' />
-						}
-					</>
-				);
-			},
-		},
-		{ accessor : 'week'  , hidden : true , excludeCSV: true },
-		{
-			accessor : 'dates',
-			sortingFn: (a, b) => {
-				if (a?.original?.week === b?.original?.week) { return 0; }
-				if (a?.original?.week > b?.original?.week) { return 1; }
-				if (a?.original?.week < b?.original?.week) { return -1; }
-				return 0;
-			},
+			header   : 'Judging Signups In',
+			accessor : 'signup',
 		},
 	];
 
@@ -249,15 +151,16 @@ const TournList = () => {
 	return (
 		<div>
 			<JSONTable
-				data    = {tournTable}
+				data    = {tourns}
 				options = {
 					{
-						transforms,
+						tableRows : tourns,
+						tableColumns,
 						customStyles,
 						striped        : true,
 						exportFileName : 'UpcomingTournament',
 						title          : 'Upcoming Tournaments',
-						getRowStyles   : (row) => (row?.original?.tournament === 'Nats' ? 'bold' : ''),
+						getRowStyles   : (row) => (row?.original?.tournament === 'Nats' ? 'semibold' : ''),
 					}
 				}
 			/>
