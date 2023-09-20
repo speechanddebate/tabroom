84cd924e89f1969e8c799489532ee2708486178c
Chris Palmer <palmer@tabroom.com>
2023-09-20 17:37:29 -0400

    Svelte lives.  getPersonHistory edits for Hardy

---
+++ svelte/src/lib/Table.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let rows;
+    export let columns;
+</script>
+
+<table id="table">
+    <thead>
+        <tr>
+            {#each columns as c}
+                <th>{c.header}</th>
+            {/each}
+        </tr>
+    </thead>
+    <tbody>
+        {#each rows as r}
+            <tr>
+                {#each columns as c}
+                    <td>{r[c.key]}</td>
+                {/each}
+            </tr>
+        {/each}
+    </tbody>
+</table>
---
+++ svelte/src/routes/+page.js
@@ -0,0 +1,11 @@
+import { tourns } from './stores.js';
+
+/** @type {import('./$types').PageLoad} */
+export const load = async () => {
+    const response = await fetch('http://local.tabroom.com/v1/invite/upcoming');
+    const data = await response.json();
+    tourns.update(() => data);
+    return {
+        tourns: data,
+    };
+};
