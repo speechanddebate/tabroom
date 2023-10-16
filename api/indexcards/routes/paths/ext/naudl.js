821eb386be0065466f6f69b79ff9780a437c6006
Chris Palmer <palmer@tabroom.com>
2023-10-16 14:18:30 -0400

    status

--- api/indexcards/routes/paths/ext/naudl.js
+++ api/indexcards/routes/paths/ext/naudl.js
@@ -1,8 +1,6 @@
 // Data posting integrations with NAUDL Salesforce
 import { getNAUDLStudents } from '../../../controllers/ext/naudl/students.js';
-import { testSalesforceConnection } from '../../../controllers/ext/naudl/conntest.js';
 
 export default [
 	{ path : '/naudl/students' , module : getNAUDLStudents } ,
-	{ path : '/naudl/salesforce' , module : testSalesforceConnection } ,
 ];
