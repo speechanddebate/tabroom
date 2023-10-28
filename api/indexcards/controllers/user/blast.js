da0f417a674553340feeecf35da89d369faef3c5
Chris Palmer <palmer@tabroom.com>
2023-10-27 20:28:41 -0400

    What is there

--- api/indexcards/controllers/user/blast.js
+++ api/indexcards/controllers/user/blast.js
@@ -6,18 +6,18 @@ export const pushMessage = {
 
 	POST: async (req, res) => {
 
-		debugLogger(req.body);
-		console.log(req.body);
+		debugLogger.info(req.body);
 
 		if (req.body.share_key !== config.SHARE_KEY) {
-			debugLogger('Invalid internal key, blast not sent');
-			console.log('does this get caught');
+			debugLogger.info('Invalid internal key, blast not sent');
 			return res.status(401).json({ message: 'Invalid internal key' });
 		}
 
 		const responseJSON = await notify({
 			...req.body,
 		});
+
+		debugLogger.info(responseJSON);
 
 		res.status(200).json(responseJSON);
 	},
