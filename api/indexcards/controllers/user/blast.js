29351e7a1789bcc67807c176e865eccd03ecd276
Chris Palmer <palmer@tabroom.com>
2023-10-27 20:52:02 -0400

    This should do it I hope

--- api/indexcards/controllers/user/blast.js
+++ api/indexcards/controllers/user/blast.js
@@ -1,23 +1,17 @@
 import notify from '../../helpers/pushNotify';
 import config from '../../../config/config';
-import { debugLogger } from '../../helpers/logger';
 
 export const pushMessage = {
 
 	POST: async (req, res) => {
 
-		debugLogger.info(req.body);
-
 		if (req.body.share_key !== config.SHARE_KEY) {
-			debugLogger.info('Invalid internal key, blast not sent');
 			return res.status(401).json({ message: 'Invalid internal key' });
 		}
 
 		const responseJSON = await notify({
 			...req.body,
 		});
-
-		debugLogger.info(responseJSON);
 
 		res.status(200).json(responseJSON);
 	},
