3d29b530ed73e89b2d113d39c1935094c6710969
Chris Palmer <palmer@tabroom.com>
2024-03-03 09:53:59 -0500

    perhaps more resilient

--- doc/utility/cleanup-cache.sh
+++ doc/utility/cleanup-cache.sh
@@ -8,12 +8,13 @@
 # deletes well, and deleting & re-creating a whole directory leads to a short
 # window that kicks errors due to the cache directory's absence.
 
+/usr/bin/chmod 1777 /www/tabroom/web/mason
 /usr/bin/mv /www/tabroom/web/mason/cache /www/tabroom/web/mason/c2
 /usr/bin/mv /www/tabroom/web/mason/obj /www/tabroom/web/mason/o2
 /usr/bin/mv /www/tabroom/web/tmp /www/tabroom/web/tmp2
 /usr/bin/mkdir /www/tabroom/web/tmp
-/usr/bin/chgrp www-data /www/tabroom/web/tmp
 /usr/bin/chmod 1777 /www/tabroom/web/tmp
+/usr/bin/chgrp www-data /www/tabroom/web/tmp
 
 # The Tabroom user should have sudo permissions to run these through the
 # ansible definitions.
