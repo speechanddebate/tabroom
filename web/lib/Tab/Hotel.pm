0186f36fb264117c0915f99723f725241bb55ff4
Chris Palmer <palmer@tabroom.com>
2024-03-04 10:47:06 -0500

    There that's better

--- web/lib/Tab/Hotel.pm
+++ web/lib/Tab/Hotel.pm
@@ -2,7 +2,7 @@ package Tab::Hotel;
 use base 'Tab::DBI';
 Tab::Hotel->table('hotel');
 Tab::Hotel->columns(Primary => "id");
-Tab::Hotel->columns(Essential => qw/tourn name multiple surcharge timestamp/);
+Tab::Hotel->columns(Essential => qw/tourn name multiple surcharge no_confirm tourn_default timestamp/);
 Tab::Hotel->has_a(tourn => 'Tab::Tourn');
 
 __PACKAGE__->_register_datetimes( qw/timestamp/);
