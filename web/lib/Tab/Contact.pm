ed987ff94fc799aa5c800f9c5fc05e3f8dde9624
Chris Palmer <palmer@tabroom.com>
2024-03-05 17:46:47 -0500

    no book is a good book

--- web/lib/Tab/Contact.pm
+++ web/lib/Tab/Contact.pm
@@ -1,7 +1,7 @@
 package Tab::Contact;
 use base 'Tab::DBI';
 Tab::Contact->table('contact');
-Tab::Contact->columns(All => qw/id school person official onsite email nsda timestamp created_at created_by/);
+Tab::Contact->columns(All => qw/id school person official onsite email nsda book timestamp created_at created_by/);
 
 Tab::Contact->has_a(school     => 'Tab::School');
 Tab::Contact->has_a(person     => 'Tab::Person');
