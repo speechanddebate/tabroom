9306275120fd5fb6db4ef0b46e910963efda138d
Chris Palmer <palmer@tabroom.com>
2024-03-26 13:52:58 -0400

    clean up cache and let cron deal

--- doc/utility/cleanup-cache.sh
+++ doc/utility/cleanup-cache.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 #
-# Moves and recycles the object cache directories, an important step because
-# old cached objects and object code can interfere with updates when the cache
-# sometimes does not properly validate.
+# Moves and recycles the object cache directory, an important step because old
+# cached objects and object code can interfere with updates sometimes
 #
 # Moved into a separate script because Ansible does not handle recursive
 # deletes well, and deleting & re-creating a whole directory leads to a short
 # window that kicks errors due to the cache directory's absence.
 
 /usr/bin/chmod 1777 /www/tabroom/web/mason
-/usr/bin/mv /www/tabroom/web/mason/cache /www/tabroom/web/mason/c2
 /usr/bin/mv /www/tabroom/web/mason/obj /www/tabroom/web/mason/o2
+
 /usr/bin/mv /www/tabroom/web/tmp /www/tabroom/web/tmp2
 /usr/bin/mkdir /www/tabroom/web/tmp
 /usr/bin/chmod 1777 /www/tabroom/web/tmp
@@ -19,7 +18,6 @@
 # The Tabroom user should have sudo permissions to run these through the
 # ansible definitions.
 
-sudo /usr/bin/rm -r /www/tabroom/web/mason/c2
 sudo /usr/bin/rm -r /www/tabroom/web/mason/o2
 sudo /usr/bin/rm -r /www/tabroom/web/tmp2
 
