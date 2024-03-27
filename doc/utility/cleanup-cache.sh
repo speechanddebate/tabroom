77212b3e88dd3098e45c3b8367f1a296807c1d9d
Chris Palmer <palmer@tabroom.com>
2024-03-27 17:19:58 -0400

    Maybe this finally fucking does it

--- doc/utility/cleanup-cache.sh
+++ doc/utility/cleanup-cache.sh
@@ -8,12 +8,15 @@
 # window that kicks errors due to the cache directory's absence.
 
 /usr/bin/chmod 1777 /www/tabroom/web/mason
-/usr/bin/chgrp www-data /www/tabroom/web/mason
+/usr/bin/chown www-data:www-data /www/tabroom/web/mason/cache
+/usr/bin/chmod 1777 /www/tabroom/web/mason/cache
+
+/usr/bin/chown www-data:www-data /www/tabroom/web/mason/obj
 /usr/bin/mv /www/tabroom/web/mason/obj /www/tabroom/web/mason/o2
 
 /usr/bin/mv /www/tabroom/web/tmp /www/tabroom/web/tmp2
 /usr/bin/mkdir /www/tabroom/web/tmp
-/usr/bin/chmod 1777 /www/tabroom/web/tmp
+/usr/bin/chmod -R 1777 /www/tabroom/web/tmp
 /usr/bin/chgrp www-data /www/tabroom/web/tmp
 
 
