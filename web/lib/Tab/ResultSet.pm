bf5514f46e19e3f0ddc9609dfb5790c9eb6397a2
Chris Palmer <palmer@tabroom.com>
2024-04-18 16:44:05 -0400

    That is much more capable

--- web/lib/Tab/ResultSet.pm
+++ web/lib/Tab/ResultSet.pm
@@ -3,7 +3,7 @@ use base 'Tab::DBI';
 Tab::ResultSet->table('result_set');
 Tab::ResultSet->columns(Primary => qw/id/);
 Tab::ResultSet->columns(Essential => qw/id tag label tourn event circuit qualifier sweep_set sweep_award
-										bracket published coach code generated timestamp/);
+										bracket published coach code generated timestamp cache/);
 
 Tab::ResultSet->has_a(tourn       => 'Tab::Tourn');
 Tab::ResultSet->has_a(event       => 'Tab::Event');
