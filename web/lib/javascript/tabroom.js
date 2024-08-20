954b908d61882803d9e054431a1a06ea85c1fe93
Chris Palmer <palmer@tabroom.com>
2024-08-20 13:41:27 -0400

    Fixed the attaway issue

--- web/lib/javascript/tabroom.js
+++ web/lib/javascript/tabroom.js
@@ -1340,7 +1340,14 @@ function hideLoginBox() {
 
 	function resizeAll() {
 
-		$('input[type=text], input[type=password], input[type=number].sizeme, input[type=email], input[type=tel], input[type=date], input[type=time], input[type=url]').each(function(){
+		$(`input[type=text],
+			input[type=password],
+			input[type=number].sizeme,
+			input[type=email],
+			input[type=tel],
+			input[type=date],
+			input[type=time],
+			input[type=url]`).each( function() {
 			if ($(this).is(':visible') && !['TD','TH','LABEL'].includes($(this).parent()[0].tagName)) {
 				$(this).width($(this).parent().width()-10);
 			}
