7c1451f814ffa8648ed59cb5267216dcb9d8aea1
Chris Palmer <palmer@tabroom.com>
2024-09-25 12:31:03 -0400

    Rating wonderland

--- web/lib/Tab/Rating.pm
+++ web/lib/Tab/Rating.pm
@@ -2,11 +2,10 @@ package Tab::Rating;
 use base 'Tab::DBI';
 Tab::Rating->table('rating');
 Tab::Rating->columns(Primary => qw/id/);
-Tab::Rating->columns(Essential => qw/tourn school entry type rating_tier judge rating_subset ordinal percentile /);
+Tab::Rating->columns(Essential => qw/tourn entry type rating_tier judge rating_subset ordinal percentile /);
 Tab::Rating->columns(Others => qw/side entered timestamp/);
 
 Tab::Rating->has_a(tourn => 'Tab::Tourn');
-Tab::Rating->has_a(school => 'Tab::School');
 Tab::Rating->has_a(entry => 'Tab::Entry');
 Tab::Rating->has_a(judge => 'Tab::Judge');
 
