dd7fa61f0e695fc34da04c636103616fa63bddbc
Chris Palmer <palmer@tabroom.com>
2024-09-27 14:23:23 -0400

    That would have been the proverbial doozy

--- web/lib/Tab/Rating.pm
+++ web/lib/Tab/Rating.pm
@@ -2,10 +2,9 @@ package Tab::Rating;
 use base 'Tab::DBI';
 Tab::Rating->table('rating');
 Tab::Rating->columns(Primary => qw/id/);
-Tab::Rating->columns(Essential => qw/tourn entry type rating_tier judge rating_subset ordinal percentile /);
+Tab::Rating->columns(Essential => qw/entry type rating_tier judge rating_subset ordinal percentile /);
 Tab::Rating->columns(Others => qw/side entered timestamp/);
 
-Tab::Rating->has_a(tourn => 'Tab::Tourn');
 Tab::Rating->has_a(entry => 'Tab::Entry');
 Tab::Rating->has_a(judge => 'Tab::Judge');
 
