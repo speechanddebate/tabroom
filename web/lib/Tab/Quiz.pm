6febc3c99527e5714670dcf13b649f8f5fd78f88
Chris Palmer <palmer@tabroom.com>
2024-09-28 22:24:42 -0400

    Fiat rules

--- web/lib/Tab/Quiz.pm
+++ web/lib/Tab/Quiz.pm
@@ -3,7 +3,7 @@ use base 'Tab::DBI';
 Tab::Quiz->table('quiz');
 Tab::Quiz->columns(Primary => qw/id/);
 Tab::Quiz->columns(Essential => qw/tag label questions description hidden
-		badge badge_link badge_description show_answers
+		badge badge_link badge_description show_answers nsda_course
 		created_at timestamp/);
 Tab::Quiz->columns(Others => qw/sitewide approval admin_only person circuit tourn/);
 
