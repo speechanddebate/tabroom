9d025ecd5a86b1b86c5d7cba5b2e1a4b2dfbb203
Chris Palmer <palmer@tabroom.com>
2024-10-16 15:19:07 -0400

    identity judge

--- web/lib/Tab/RoomStrike.pm
+++ web/lib/Tab/RoomStrike.pm
@@ -13,17 +13,19 @@ Tab::RoomStrike->has_a(judge => 'Tab::Judge');
 __PACKAGE__->_register_datetimes( qw/timestamp/);
 __PACKAGE__->_register_datetimes( qw/start end/);
 
-sub name { 
+sub name {
 	my $self = shift;
 	my $tz = $self->tourn->tz if $self->tourn;
 	$tz = "UTC" unless $tz;
 
-	return "No rounds of ".$self->event->name if $self->type eq "event";
+	if ($self->type eq "event" && $self->event > 0) {
+		return "No rounds of ".$self->event->name;
+	};
 
-	return Tab::niceshortdayt($self->start->set_time_zone($tz)) ." until ".  Tab::niceshortdayt($self->end->set_time_zone($tz)) 
+	return Tab::niceshortdayt($self->start->set_time_zone($tz)) ." until ".  Tab::niceshortdayt($self->end->set_time_zone($tz))
 		if $self->type eq "time" && $self->start->day != $self->end->day;
 
-	return Tab::niceshortdayt($self->start->set_time_zone($tz)) ." until ".  Tab::nicetime($self->end->set_time_zone($tz)) 
+	return Tab::niceshortdayt($self->start->set_time_zone($tz)) ." until ".  Tab::nicetime($self->end->set_time_zone($tz))
 		if $self->type eq "time" && $self->start->day == $self->end->day;
 
 	return "No judge ".$self->judge->code." ".$self->judge->first." ".$self->judge->last if $self->type eq "judge";
