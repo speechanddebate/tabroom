3df5e7b79de365791807a42ce8600b6671e9cfca
Chris Palmer <palmer@tabroom.com>
2025-05-24 06:40:52 -0400

    Practice makes perfect

--- web/lib/Tab/Practice.pm
+++ web/lib/Tab/Practice.pm
@@ -1,7 +1,7 @@
 package Tab::Practice;
 use base 'Tab::DBI';
 Tab::Practice->table('practice');
-Tab::Practice->columns(Essential => qw/id name start end reported created_at created_by chapter timestamp /);
+Tab::Practice->columns(Essential => qw/id name tag start end reported created_at created_by chapter timestamp /);
 Tab::Practice->has_a(chapter => 'Tab::Chapter');
 Tab::Practice->has_a(created_by => 'Tab::Person');
 Tab::Practice->has_many(students => [Tab::PracticeStudent => 'student']);
