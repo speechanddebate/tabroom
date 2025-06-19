3b63cb7e3a2dba0b4d42a71954d938110f09d7b2
Chris Palmer <palmer@tabroom.com>
2025-06-18 21:57:20 -0400

    Whatever possessed you to do that.

--- web/lib/Tab/Email.pm
+++ web/lib/Tab/Email.pm
@@ -2,8 +2,9 @@ package Tab::Email;
 use base 'Tab::DBI';
 Tab::Email->table('email');
 Tab::Email->columns(Primary => qw/id/);
-Tab::Email->columns(Essential => qw/sender sender_raw content metadata subject sent_at sent_to hidden circuit tourn timestamp/);
+Tab::Email->columns(Essential => qw/sender person sender_raw content metadata subject sent_at sent_to hidden circuit tourn timestamp/);
 Tab::Email->has_a(sender => 'Tab::Person');
+Tab::Email->has_a(person => 'Tab::Person');
 Tab::Email->has_a(tourn => 'Tab::Tourn');
 
 __PACKAGE__->_register_datetimes( qw/sent_at timestamp/);
